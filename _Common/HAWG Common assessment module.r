58b21c40e34a9470c406e1b60575232adb8e1924
mpa <mpa@aqua.dtu.dk>
2009-02-18 00:13:53 +0000

    Common: Added new QuantMean function until bug FS#3 is fixed in FLCore

--- _Common/HAWG Common assessment module.r
+++ _Common/HAWG Common assessment module.r
@@ -352,4 +352,5 @@ if(compareVersion(paste(version$major,version$minor,sep="."),required.version)==
 
 #Add in other functions
 source(file.path("..","_Common","HAWG Retro func.r"))
+source(file.path("..","_Common","HAWG QuantMean.r"))
 
