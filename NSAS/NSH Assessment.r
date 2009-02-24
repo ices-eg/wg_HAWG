27087b01c2e341f7302c42cf14a04ddb8ebea470
mpa <mpa@aqua.dtu.dk>
2009-02-24 15:07:04 +0000

    NSAS: Removed forcing path

--- NSAS/NSH Assessment.r
+++ NSAS/NSH Assessment.r
@@ -34,8 +34,8 @@
 ### ======================================================================================================
 rm(list=ls()); gc(); graphics.off(); start.time <- proc.time()[3]
 
-path <- "N:/Projecten/ICES WG/Haring werkgroep HAWG/2009/assessment/googlecode/NSAS/"
-setwd(path)
+#path <- "N:/Projecten/ICES WG/Haring werkgroep HAWG/2009/assessment/googlecode/NSAS/"
+#setwd(path)
 
 options(stringsAsFactors=FALSE)
 FnPrint     <-  function(string) {
