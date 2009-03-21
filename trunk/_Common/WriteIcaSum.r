326bff1fe43ec5718597d0b60e18569dfd892749
mpa@aqua.dtu.dk <mpa@aqua.dtu.dk@3861ee22-ec89-11dd-a738-5fe2e3cfbcc5>
2009-03-21 17:55:09 +0000

    Common: Write ICA sum working

--- trunk/_Common/WriteIcaSum.r
+++ trunk/_Common/WriteIcaSum.r
@@ -1,3 +1,14 @@
+## writeFLStock	{{{
+writeFLStock <- function(FLStock, output.file=FLStock@name, type="VPA") {
+	if (!inherits(FLStock, "FLStock"))
+		stop("FLStock must be an 'FLStock' object!")
+	switch(type,
+		"VPA" = writeVPA(FLStock, output.file),
+		"ICA" = writeICAsum(FLStock, output.file),
+		stop("type must be either 'VPA' or 'ICA'!"))
+}	# }}}
+
+
 writeICAsum <- function(stk,fname) {
     #Setup header - copied directly from the heart of ICA
     hdr <-  rbind(" ³ Year ³  Recruits  ³  Total  ³ Spawning³ Landings ³ Yield ³ Mean F ³ SoP ³",
@@ -33,5 +44,101 @@ writeICAsum <- function(stk,fname) {
     write(output,fname)
 }
 
+# writeVPA - Mark Payne, DTU-Aqua		{{{
+writeVPA <- function(FLStock, output.file=FLStock@name,slots="missing") {
+    #Check for dimensions that Lowestoft VPA can't handle
+    if(dims(FLStock)$iter >1) stop("FLStock object contains more than
+one iteration. Lowestoft VPA can't handle this unfortunately.")
+    if(dims(FLStock)$area >1) stop("FLStock object contains more than
+one area. Lowestoft VPA can't handle this unfortunately.")
+    if(dims(FLStock)$season >1) stop("FLStock object contains more than
+one season. Lowestoft VPA can't handle this unfortunately.")
+    if(dims(FLStock)$unit >1) stop("FLStock object contains more than
+one unit. Lowestoft VPA can't handle this unfortunately.")
+
+    #List of configuration information
+    config.df  <-  rbind(
+      c(1, "landings", "LATON", "total landings"),
+      c(2, "landings.n", "LANUM", "landings-at-age"),
+      c(3, "landings.wt", "WELAND", "landings weight-at-age"),
+      c(4, "stock.wt", "WEST", "stock weight-at-age"),
+      c(5, "m", "NATMOR", "natural mortality"),
+      c(6, "mat", "MATPROP", "maturity-at-age ogive"),
+      c(7, "harvest.spwn", "FPROP", "proportion of F before spawning"),
+      c(8, "m.spwn", "MPROP", "proportion of M before spawning"),
+      c(12,"harvest", "F", "fishing mortality"),
+      c(13,"stock.n", "N", "stock numbers at age"),
+      c(21, "discards", "DISTON", "total discards"),
+      c(22, "discards.n", "DISNUM", "discards-at-age"),
+      c(23, "discards.wt", "WEDIS", "discards weight-at-age"),
+      c(24, "catch", "CATON", "total catch"),
+      c(25, "catch.n", "CANUM", "catch-at-age"),
+      c(26, "catch.wt", "WECA", "catch weight-at-age"))
+    colnames(config.df) <-  c("idx","stock.slot","file.ext","desc")
+
+    config.df   <- as.data.frame(config.df)
+
+    #Only write the harvest slot if units are f.
+    if(!any(units(FLStock@harvest) %in% c("f","F")))
+    {
+      config.df <- config.df[-which(config.df$stock.slot=="harvest"),]
+      warning("Harvest slot is not a fishing mortality - it will not
+        be written as part of the output.")
+    }
+
+    #calculates the numbers of ages
+    nage  <- FLStock@range[2]-FLStock@range[1]+1
+    #calculates the number of years
+    nyear <- dims(FLStock)$year
+
+    #Function to write files. Based on original code by David Bromley, CEFAS
+    writeVPAFile <- function(file.ext,stock.slot,idx,desc)
+    {
+      # handles the annoying "strings as factors" option
+      stock.slot <- as.character(stock.slot)
+      # open the output file connections
+      temp <- try(file(paste(output.file,"-",file.ext,".txt",sep=""),
+        "w"),silent=TRUE)
+      if(is(temp,"try-error"))
+      {
+        stop(paste("Cannot open output file. The supplied name,
+        \"",output.file,"\", may not be a valid filename. Try setting the
+        output.file argument.",sep=""))
+        }
+        # adds the VPA format info to the begining of each file
+        cat(paste(FLStock@name, desc, "-",stock.slot,
+          "(units :",units(slot(FLStock,stock.slot)),")\n"), file=temp)
+        cat(1, idx,"\n", file=temp, sep="\t")
+        cat(FLStock@range["minyear"], FLStock@range["maxyear"], "\n",
+          file=temp, sep="\t")
+        cat(FLStock@range["min"], FLStock@range["max"], "\n", file=temp,
+          sep="\t")
+        cat(1,"\n",  file=temp)
+        # append the data to the file
+ 			  write(matrix(slot(FLStock,stock.slot), nrow=nage, ncol=nyear),
+          ncolumns=nage, file=temp)
+        close(temp)
+    }
+
+    #Now write the data out
+    if(missing(slots))
+    {
+      #Write all the data out, and an index file
+      #Write the slots sequentially
+      do.call(mapply,list(config.df$file.ext, config.df$stock.slot,
+        config.df$idx, config.df$desc, FUN=writeVPAFile))
+      # produces the index file
+      temp <- file(paste(output.file, "-INDEX.txt", sep=""), "w")
+      cat(FLStock@name, "\n", file=temp)
+      cat(1,paste(output.file,"-",config.df$file.ext,".txt",sep=""),
+        file=temp, sep="\n")
+      close(temp)
+    } else
+    {
+      output.config <-  config.df[config.df$stock.slot %in% slots,]
+      do.call(mapply,c(as.list(output.config),FUN=writeVPAFile))
+    }
+    return(invisible(NULL))
+}	# }}}
 
 
