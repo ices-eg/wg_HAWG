54925c32e76fbd4a7df509448bd8792e4ae3fc84
mpa@aqua.dtu.dk <mpa@aqua.dtu.dk@3861ee22-ec89-11dd-a738-5fe2e3cfbcc5>
2009-03-21 17:33:59 +0000

    Common: Upgrade to FLICA 1.4-10

--- trunk/_Common/HAWG Common assessment module.r
+++ trunk/_Common/HAWG Common assessment module.r
@@ -479,7 +479,7 @@ check.versions <-  function(lib,ver,required.date="missing"){
 }
 check.versions("FLCore","3.0",ISOdatetime(2009,03,10,04,42,27))
 check.versions("FLAssess","1.99-102")
-check.versions("FLICA","1.4-9")
+check.versions("FLICA","1.4-10")
 check.versions("FLSTF","1.99-1")
 check.versions("FLEDA","2.0")
 check.versions("FLBRP","2.0")
