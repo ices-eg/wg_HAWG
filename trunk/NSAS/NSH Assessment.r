01046ae19dd761ffa22241db6fa034b9685f036e
Niels.Hintzen <Niels.Hintzen@3861ee22-ec89-11dd-a738-5fe2e3cfbcc5>
2010-03-18 18:03:10 +0000

    Adding some more plots, like the timeseries plot, and added more comments to explain the code. Some other cosmetic changes to the plotting too.

--- trunk/NSAS/NSH Assessment.r
+++ trunk/NSAS/NSH Assessment.r
@@ -5,16 +5,16 @@
 # $Date$
 #
 # Author: Niels Hintzen
-# Wageningen IMARES, The Netherlands
+# IMARES, The Netherlands
 # With great compliments to M.Payne, DTU-aqua
 #
 # Performs an assessment of Western Baltic Spring Spawning Herring (NSH) in IIIa using the
 # FLICA package.
 #
 # Developed with:
-#   - R version 2.8.0
-#   - FLCore 3.0
-#   - FLICA, version 1.4-8
+#   - R version 2.8.1
+#   - FLCore 2.2
+#   - FLICA, version 1.4-12
 #   - FLAssess, version 1.99-102
 #   - FLSTF, version 1.99-1
 #   - FLEDA, version 2.0
@@ -63,6 +63,9 @@ n.retro.years       <-  10                                      #Number of years
 ### Output setup
 ### ======================================================================================================
 png(paste(output.base,"figures - %02d.png"),units = "px", height=1200,width=800,pointsize = 24, bg = "white")
+#png(paste(output.base,"figures - 64.png"),units = "px", height=1200,width=800,pointsize = 24, bg = "white")
+
+
 #Set default lattice fontsize, so that things are actually readible!
 trellis.par.set(fontsize=list(text=24,points=20))
 
@@ -100,7 +103,7 @@ units(NSH)[1:17]                  <- as.list(c(rep(c("tonnes","thousands","kg"),
 range(NSH)[c("minfbar","maxfbar")]<- c(2,6)
 
 #- Set stock object name - this is propagated through into the figure titles
-NSH@name                          <- "Autumn spawning herring in IV, VIId, IIIa"
+NSH@name                          <- "North Sea Herring"
 
 #- Set plus group
 NSH                               <- setPlusGroup(NSH,NSH@range["max"])
@@ -118,7 +121,7 @@ FnPrint("PREPARING INDEX OBJECT...\n")
 NSH.tun                         <- readFLIndices(file.path(data.source,"/fleet.txt"),file.path(data.source,"/ssb.txt"),type="ICA")
 
 #Load the NSH.tun with the scai rather than the MLAI (exploratory in 2010)
-NSH.tun                         <- readFLIndices(file.path(data.source,"/fleet.txt"),file.path(data.source,"/scai.txt"),type="ICA")
+#NSH.tun                         <- readFLIndices(file.path(data.source,"/fleet.txt"),file.path(data.source,"/scai.txt"),type="ICA")
 
 #Set names, and parameters etc
 NSH.tun[[1]]@type               <- "number"
@@ -150,6 +153,8 @@ NSH@stock                       <-  computeStock(NSH)
 ### Use the standard code from the common modules to produce outputs
 ### ======================================================================================================
 do.summary.plots(NSH,NSH.ica)
+# Extra otolith plot with a slightly higher number of samples in there
+plot.otolith(NSH,NSH.ica,n=100000)
 NSH.retro <- do.retrospective.plots(NSH,NSH.tun,NSH.ctrl,n.retro.years)
 
 ### ======================================================================================================
@@ -157,45 +162,55 @@ NSH.retro <- do.retrospective.plots(NSH,NSH.tun,NSH.ctrl,n.retro.years)
 ### ======================================================================================================
 FnPrint("GENERATING CUSTOM PLOTS...\n")
 
+# Plot the mature and immature part of the stock
 mat.immat.ratio(NSH)
-cpue.survey(NSH.tun,"index") 
+# Plot the cpue for each survey against each other to see if they get through the same signals
+cpue.survey(NSH.tun,"index")
 
-print(stacked.area.plot(data~year| unit, as.data.frame(pay(NSH@stock.n)),groups="age",main="Proportion of stock.n at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
-print(stacked.area.plot(data~year| unit, as.data.frame(pay(NSH@catch.n)),groups="age",main="Proportion of Catch.n at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
-print(stacked.area.plot(data~year| unit, as.data.frame(pay(NSH@catch.wt)),groups="age",main="Proportion of Catch.wt at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
-print(stacked.area.plot(data~year,as.data.frame(pay(NSH@stock.n*NSH@stock.wt)),groups="age",main="Proportion by weight in the stock",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
-print(stacked.area.plot(data~age| unit, as.data.frame(pay(NSH@catch.n)),groups="year",main="Total Historic Catches from an age group",xlab="years",col=gray(9:0/9)))
+# Plot the proportion of catch and weight in numbers and weight to see if the catch is representative for the stock build-up
+print(stacked.area.plot(data~year| unit, as.data.frame(pay(NSH@stock.n)),groups="age",main="Proportion of Stock numbers at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
+print(stacked.area.plot(data~year| unit, as.data.frame(pay(NSH@catch.n)),groups="age",main="Proportion of Catch numbers at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
+print(stacked.area.plot(data~year| unit, as.data.frame(pay(NSH@stock.wt)),groups="age",main="Proportion of Stock weight at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
+print(stacked.area.plot(data~year| unit, as.data.frame(pay(NSH@catch.wt)),groups="age",main="Proportion of Catch weight at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
 
+# Plot the harvest pattern at age as a proportion over time
+print(stacked.area.plot(data~year| unit, as.data.frame(pay(NSH@harvest)),groups="age",main="Proportion of harvest pressure at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
+
+# Plot the proportion of catch in numbers in the indices to see if the indices are having specific yearclass trends
 print(stacked.area.plot(data~year| unit, as.data.frame(pay(NSH.tun[[3]]@index)),groups="age",main="Proportion of IBTS index at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
 print(stacked.area.plot(data~year| unit, as.data.frame(pay(NSH.tun[[4]]@index)),groups="age",main="Proportion of Acoustic index at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
 
+# Plot the catch curves to see if there is a change in the selectivity of the fleet (seperable period) or the age which is targetted the most (seperable age)
 catch.curves(NSH,1990,2009)
+
+# Plot the reference points on yield - F curves, and print the reference points
 NSH.sr <- ref.pts(NSH,"bevholt",100000)
+#NSH.sr <- ref.pts(NSH,"ricker",100000)
+
+# Create a co-plot of the tuning indices, and see if there is correlation between age groups in the survey (there should be)
 cor.tun(NSH.tun)
 
+# Plot the historic indices updated with new values against each other to see if similar patterns are coming through
 source("./private_diagnostics.r")
-LNV.fbar(NSH,0.25,0.1,c(2,6))
-LNV.fbar(NSH,0.1,0.04,c(0,1))
-LNV.ssb(NSH,1.5e6,0.8e6)
-LNV.rec(NSH,NSH.ica)     
+#LNV.fbar(NSH,0.25,0.1,c(2,6))
+#LNV.fbar(NSH,0.1,0.04,c(0,1))
+#LNV.ssb(NSH,1.5e6,0.8e6)
+#LNV.rec(NSH,NSH.ica)
 
+# Calculate the stock recruitment fit, and plot it, and also add years to the plot
 NSH.sr <- fmle(as.FLSR(transform(NSH,stock.n=NSH@stock.n/100000),model="bevholt")); 
 plot(NSH.sr)
 NSH.sr@params <- NSH.sr@params*100000
-plot(NSH.sr@rec[,-1]~NSH.sr@ssb[,1:49],type="b",xlab="SSB",ylab="Rec",main="Yearly stock recruitment relationship")
-text(NSH.sr@rec[,-1]~NSH.sr@ssb[,1:49],labels=dimnames(NSH.sr@rec)$year[-1],pos=1,cex=0.7)
+plot(NSH.sr@rec[,-1]~NSH.sr@ssb[,1:c(length(dimnames(NSH.sr@ssb)$year)-1)],type="b",xlab="SSB",ylab="Rec",main="Yearly stock recruitment relationship")
+text(NSH.sr@rec[,-1]~NSH.sr@ssb[,1:c(length(dimnames(NSH.sr@ssb)$year)-1)],labels=dimnames(NSH.sr@rec)$year[-1],pos=1,cex=0.7)
 
-#Time series of west
-west.ts  <- xyplot(data~year,data=window(NSH@stock.wt,1975,2009),
-              groups=age,
-              auto.key=list(space="right",points=FALSE,lines=TRUE,type="b"),
-              type="b",
-              xlab="Year",ylab="Weight in the stock (kg)",
-              main=paste(NSH@name,"Weight in the Stock"),
-              par.settings=list(superpose.symbol=list(pch=as.character(0:8),cex=1.25)))
-print(west.ts)
+# Plot the time series of weight in the stock and catch in the stock
+timeseries(window(NSH,1975,2009),slot="stock.wt")
+timeseries(window(NSH,1975,2009),slot="catch.wt")
+timeseries(window(NSH,2000,2009),slot="harvest")
+timeseries(window(NSH,1990,2009),slot="mat")
 
-#Time series of west anomalies
+#Time series of weigth of the stock anomalies
 anom.plot(trim(NSH@stock.wt,year=1983:dims(NSH)$maxyear,age=0:1),xlab="Year",ylab="Anomaly (std. devs)",
     main=paste(NSH@name,"Weight in the Stock Anomaly (Age 0-1)"),ylim=c(-3,3))
 anom.plot(trim(NSH@stock.wt,year=1983:dims(NSH)$maxyear,age=3:6),xlab="Year",ylab="Anomaly (std. devs)",
@@ -203,7 +218,7 @@ anom.plot(trim(NSH@stock.wt,year=1983:dims(NSH)$maxyear,age=3:6),xlab="Year",yla
 
 #Time series of west by cohort
 west.by.cohort      <- as.data.frame(FLCohort(window(NSH@stock.wt,1980,2009)))
-west.by.cohort      <-  subset(west.by.cohort,!is.na(west.by.cohort$data))
+west.by.cohort      <- subset(west.by.cohort,!is.na(west.by.cohort$data))
 west.by.cohort$year <- west.by.cohort$age + west.by.cohort$cohort
 west.cohort.plot    <- xyplot(data~year,data=west.by.cohort,
               groups=cohort,
@@ -218,6 +233,18 @@ west.cohort.plot    <- xyplot(data~year,data=west.by.cohort,
               })
 print(west.cohort.plot)
 
+# Plot the TAC's versus the realized catches. The TAC's have to be added manually
+par(oma=c(rep(2,4)))
+TACs          <- data.frame(year=1987:2009,TAC=c(600,530,514,415,420,430,430,440,440, 156+44,159+24,254+22,265+30,265+36,265+36,265+36,400+52,460+38,535+50,455+43,341+32,201+19,171))
+TAC.plot.dat  <- data.frame(year=rep(TACs$year,each=2)+c(-0.5,0.5),TAC=rep(TACs$TAC,each=2))
+catch         <- as.data.frame(NSH@catch[,ac(1987:2009)]/1e3)
+plot(0,0,pch=NA,xlab="Year",ylab="Catch",xlim=range(c(catch$year,TAC.plot.dat$year)),ylim=range(c(0,TAC.plot.dat$TAC,catch$data)),cex.lab=cl,cex.axis=ca,family=fam,font=fonts)
+rect(catch$year-0.5,0,catch$year+0.5,catch$data,col="grey")
+lines(TAC.plot.dat,lwd=3)
+legend("topright",legend=c("Catch","TAC"),lwd=c(1,5),lty=c(NA,1),pch=c(22,NA),col="black",pt.bg="grey",pt.cex=c(2),box.lty=0)
+box()
+title(main=paste(NSH@name,"Catch and TAC"))
+mtext("Working group estimate",side=1,outer=F,line=5,cex=ca)
 
 
 ### ======================================================================================================
@@ -240,7 +267,7 @@ writeFLStock(NSH,output.file=output.base)
 ### ======================================================================================================
 FnPrint("PERFORMING SHORT TERM FORECAST...\n")
 REC               <- NSH.ica@param["Recruitment prediction","Value"]
-TAC               <- 165000 #overshoot = approximately 13% every year + 1000 tons of transfer             #194233 in 2009
+TAC               <- 164300 #overshoot = approximately 13% every year + 1000 tons of transfer             #194233 in 2009  #It does not matter what you fill out here as it only computes the suvivors
 NSH.stf           <- FLSTF.control(fbar.min=2,fbar.max=6,nyrs=1,fbar.nyrs=1,f.rescale=TRUE,rec=REC,catch.constraint=TAC)
 NSH.stock10       <- as.FLStock(FLSTF(stock=NSH,control=NSH.stf,unit=1,season=1,area=1,survivors=NA,quiet=TRUE,sop.correct=FALSE))
 
@@ -257,6 +284,8 @@ points(y=fbar(NSH.stock10[,ac(2002:2010)]), x=(ssb(NSH.stock10[,ac(2002:2010)])/
 lines(y=fbar(NSH.stock10[,ac(2002:2010)]),  x=(ssb(NSH.stock10[,ac(2002:2010)])/1e6))
 text(y=fbar(NSH.stock10[,ac(2002:2010)]),   x=(ssb(NSH.stock10[,ac(2002:2010)])/1e6),labels=ac(2002:2010),pos=3,cex=0.7)
 
+
+
 #Write the stf results out in the lowestoft VPA format for further analysis eg MFDP
 writeFLStock(NSH.stock10,output.file=paste(output.base,"with STF"))
 
@@ -264,7 +293,7 @@ writeFLStock(NSH.stock10,output.file=paste(output.base,"with STF"))
 ### Save workspace and Finish Up
 ### ======================================================================================================
 FnPrint("SAVING WORKSPACES...\n")
-save(NSH,NSH.stock09,NSH.tun,NSH.ctrl,file=paste(output.base,"Assessment.RData"))
+save(NSH,NSH.stock10,NSH.tun,NSH.ctrl,file=paste(output.base,"Assessment.RData"))
 save.image(file=paste(output.base,"Assessment Workspace.RData"))
 dev.off()
 FnPrint(paste("COMPLETE IN",sprintf("%0.1f",round(proc.time()[3]-start.time,1)),"s.\n\n"))
