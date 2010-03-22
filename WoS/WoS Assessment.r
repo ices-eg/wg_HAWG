e1b763775bd5e64b2cf33ca0714c5256b2e845b0
Niels.Hintzen <Niels.Hintzen@3861ee22-ec89-11dd-a738-5fe2e3cfbcc5>
2010-03-22 14:36:59 +0000

    Tidying up the code and replacing recruitment in 2009 for geometric mean. Getting rid of all the rounding in the code

--- WoS/WoS Assessment.r
+++ WoS/WoS Assessment.r
@@ -101,21 +101,26 @@ FnPrint("PREPARING INDEX OBJECT...\n")
 WoS.tun   <- readFLIndices(file.path(data.source, "fleet.txt"))
 
 #Set names, and parameters etc
-WoS.tun[[1]]@index.var[] <- 1
-WoS.tun[[1]]@index.var["1",] <- 10
-WoS.tun[[1]]@type <- "number"
-names(WoS.tun) <- "WoS Summer Acoustic Survey"  #MPA: Added so that your graphs are a bit prettier
+WoS.tun[[1]]@index.var[]      <- 1
+WoS.tun[[1]]@index.var["1",]  <- 10
+WoS.tun[[1]]@type             <- "number"
+names(WoS.tun)                <- "WoS Summer Acoustic Survey"  #MPA: Added so that your graphs are a bit prettier
 #Set units
-units(WoS.tun[[1]])[1:7] <- as.list(c("number","NA","number","Kg","NA","NA","NA"))
+units(WoS.tun[[1]])[1:7]      <- as.list(c("number","NA","number","Kg","NA","NA","NA"))
 
 ### ======================================================================================================
 ### Perform the assessment
 ### ======================================================================================================
 FnPrint("PERFORMING ASSESSMENT...\n")
 #Now perform the asssessment
-WoS.ica   <-  FLICA(WoS,WoS.tun,WoS.ctrl)
-WoS       <-  WoS + WoS.ica
-WoS@stock=computeStock(WoS) # to get TSB in stock slot
+WoS.ica   <- FLICA(WoS,WoS.tun,WoS.ctrl)
+WoS       <- WoS + WoS.ica
+WoS@stock <- computeStock(WoS) # to get TSB in stock slot
+
+WoS@stock.n[1,ac(2009)] <- NA #Take this recruitment out because it is not well estimated in ICA as there is no information to support it
+
+#-Replace the recruitment value in 2009 with a geometric mean recruitment over the years 1986 to 2007
+WoS@stock.n[1,ac(2009)] <- exp(mean(log(rec(WoS[,ac(1989:2007)])),na.rm=T))
 
 ### ======================================================================================================
 ### Use the standard code from the common modules to produce outputs
@@ -129,10 +134,12 @@ do.retrospective.plots<- function(stck,idxs,ctrl,n.retro.yrs) {
     cat("GENERATING RETROSPECTIVE ANALYSES...\n");flush.console()
 
     #Generate a retrospective analysis
-    icas <- retro(stck,idxs,ctrl,retro=n.retro.yrs,return.FLStocks=FALSE)
-    stcks <- do.call(FLStocks,lapply(icas,function(ica) {ica + trim(stck, year=dims(ica@stock.n)$minyear:dims(ica@stock.n)$maxyear)}))
-# this line to get rid of the last recruitment value
+    icas    <- retro(stck,idxs,ctrl,retro=n.retro.yrs,return.FLStocks=FALSE)
+    stcks   <- do.call(FLStocks,lapply(icas,function(ica) {ica + trim(stck, year=dims(ica@stock.n)$minyear:dims(ica@stock.n)$maxyear)}))
+
+    # Remove last years recruitment value as it gets estimated wrong in ICA, replace it later with a GM estimate
     stcks[[9]]@stock.n[1,ac(2009)] <- NA
+    stcks[[9]]@stock.n[1,ac(2009)] <- exp(mean(log(rec(stck[,ac(1989:2007)])),na.rm=T))
     #Now call the retro plotting functions
     retro.plots(stcks,icas,ctrl)
 
@@ -162,7 +169,7 @@ print(stacked.area.plot(data~year,as.data.frame(pay(WoS@stock.n*WoS@stock.wt)),g
 print(stacked.area.plot(data~year| unit, as.data.frame(pay(WoS.tun[[1]]@index)),groups="age",main="Proportion of Acoustic index at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
 
 catch.curves(WoS,1990,2009)
-WoS.sr <- ref.pts(WoS,"bevholt",100000)
+WoS.sr <- ref.pts(WoS,"segreg",100000)
 cor.tun(WoS.tun)
 
 #Time series of west
@@ -177,7 +184,7 @@ print(west.ts)
 
 #Time series of west by cohort
 west.by.cohort      <- as.data.frame(FLCohort(window(WoS@stock.wt,1992,2009)))
-west.by.cohort      <-  subset(west.by.cohort,!is.na(west.by.cohort$data))
+west.by.cohort      <- subset(west.by.cohort,!is.na(west.by.cohort$data))
 west.by.cohort$year <- west.by.cohort$age + west.by.cohort$cohort
 west.cohort.plot    <- xyplot(data~year,data=west.by.cohort,
               groups=cohort,
@@ -200,21 +207,7 @@ FnPrint("GENERATING DOCUMENTATION...\n")
 #Document the run with alternative table numbering and a reduced width
 old.opt <- options("width","scipen")
 options("width"=80,"scipen"=1000)
-#Do some tidying up on precision of the ica file
-WoS.orig=WoS
-WoS.ica@catch.res@.Data <- round(WoS.ica@catch.res@.Data,3)
-WoS.ica@index.res[[1]]@.Data <- round(WoS.ica@index.res[[1]]@.Data,3)
-WoS.ica@survivors=round(WoS.ica@survivors)
-WoS.ica@sel=round(WoS.ica@sel,3)
-WoS@harvest <- zapsmall(WoS@harvest,3)
-WoS@stock.n=round(WoS@stock.n)
-WoS@catch.n=round(WoS@catch.n)
-WoS.ica@catch.n=round(WoS.ica@catch.n)
-WoS.ica@index.hat[[1]]@.Data=round(WoS.ica@index.hat[[1]]@.Data)
-WoS@mat=round(WoS@mat,2)
-WoS@stock.wt=round(WoS@stock.wt,3)
-WoS@catch.wt=round(WoS@catch.wt,3)
-WoS.ica@param[,6:10]=round(WoS.ica@param[6:10],2)
+
 
 #Now write the file
 ica.out.file <- ica.out(WoS,WoS.tun,WoS.ica,format="TABLE 5.6.2.%i HERRING in VIa (N).")
@@ -222,24 +215,23 @@ write(ica.out.file,file=paste(output.base,"ica.out",sep="."))
 options("width"=old.opt$width,"scipen"=old.opt$scipen)
 
 #And finally, write the results out in the lowestoft VPA format for further analysis eg MFDP
-writeFLStock(WoS.orig,output.file=output.base)
+writeFLStock(WoS,output.file=output.base)
  
 ### ======================================================================================================
 ### Short Term Forecast
 ### ======================================================================================================
 FnPrint("PERFORMING SHORT TERM FORECAST...\n")
 #Make forecast
-gm.recs         <- exp(mean(log(rec(trim(WoS.orig,year=1989:2007)))))  
 #WoS recruitment is based on a geometric mean of 1989-2007. Updated by one year, each year
-stf.ctrl        <- FLSTF.control(nyrs=1,fbar.nyrs=1,fbar.min=3,fbar.max=6,catch.constraint=24420,f.rescale=TRUE,rec=gm.recs)
-WoS.orig@stock.n["1",ac(2009)] <- gm.recs
-WoS.orig@catch.n[1,52,,,,]=1
-WoS.stf        <- FLSTF(stock=WoS.orig,control=stf.ctrl,quiet=TRUE,sop.correct=FALSE)
-writeFLStock(WoS.stf,output.file="WoSaddyr")
+gm.recs                   <- exp(mean(log(rec(trim(WoS.orig,year=1989:2007)))))
+stf.ctrl                  <- FLSTF.control(nyrs=1,fbar.nyrs=1,fbar.min=3,fbar.max=6,catch.constraint=24420,f.rescale=TRUE,rec=gm.recs)
+WoS@catch.n[1,52,,,,]     <- 1
+WoS.stf                   <- FLSTF(stock=WoS.orig,control=stf.ctrl,quiet=TRUE,sop.correct=FALSE)
+writeFLStock(WoS.stf,output.file=paste(output.base," WoSaddyr",sep=""))
 ## use the rounder version so report and quality control database have same values
 writeFLStock(WoS,file.path(output.dir,"hawg_her-vian.sum"),type="ICAsum")
 # project one year in order to get a single year holding means for YPR output
-WoS.proj=stf(WoS.orig,nyears=1,wts.nyears=3,fbar.nyears=1,arith.mean=TRUE,na.rm=TRUE)
+WoS.proj                  <- stf(WoS.orig,nyears=1,wts.nyears=3,fbar.nyears=1,arith.mean=TRUE,na.rm=TRUE)
 writeFLStock(WoS.proj,file.path(output.dir,"hawg_her-vian.ypr"),type="YPR")
 
 # Write the stf results out in the lowestoft VPA format for further analysis eg MFDP
@@ -249,7 +241,7 @@ writeFLStock(WoS.stf,output.file=paste(output.base,"with STF"))
 ### Save workspace and Finish Up
 ### ======================================================================================================
 FnPrint("SAVING WORKSPACES...\n")
-save(WoS.orig,WoS.tun,WoS.ctrl,file=paste(output.base,"Assessment.RData"))
+save(WoS,WoS.tun,WoS.ctrl,file=paste(output.base,"Assessment.RData"))
 save.image(file=paste(output.base,"Assessment Workspace.RData"))
 dev.off()
 FnPrint(paste("COMPLETE IN",sprintf("%0.1f",round(proc.time()[3]-start.time,1)),"s.\n\n"))
@@ -258,7 +250,7 @@ FnPrint(paste("COMPLETE IN",sprintf("%0.1f",round(proc.time()[3]-start.time,1)),
 ### Create the figures for the advice sheet and the summary table and reference points
 ### ======================================================================================================
 WoS.sr <- fmle(FLSR(rec=rec(WoS),ssb=ssb(WoS),model="bevholt"),control=list(parscale=c(0.001,1,0.001)))
-WoS.sr <- fmle(as.FLSR(transform(WoS,stock.n=WoS@stock.n/100000),model="bevholt"));
+#WoS.sr <- fmle(as.FLSR(transform(WoS,stock.n=WoS@stock.n/100000),model="segreg"));
 WoS.sr@params <- WoS.sr@params*100000
 
 # Or, for Hockey-Stick
