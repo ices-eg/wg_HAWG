d885f85882fa9cf07f887aab14fcad5546406852
e.hatfield <e.hatfield@marlab.ac.uk>
2011-03-07 12:54:31 +0000

    Replaced 2009 with 2010 where appropriate

--- WoS/WoS Assessment.r
+++ WoS/WoS Assessment.r
@@ -119,15 +119,15 @@ WoS@stock <- computeStock(WoS) # to get TSB in stock slot
 
 WoS@stock.n[1,ac(2009)] <- NA #Take this recruitment out because it is not well estimated in ICA as there is no information to support it
 
-#-Replace the recruitment value in 2009 with a geometric mean recruitment over the years 1986 to 2007
-WoS@stock.n[1,ac(2009)] <- exp(mean(log(rec(WoS[,ac(1989:2007)])),na.rm=T))
+#-Replace the recruitment value in 2010 with a geometric mean recruitment over the years 1986 to 2007
+WoS@stock.n[1,ac(2010)] <- exp(mean(log(rec(WoS[,ac(1989:2007)])),na.rm=T))
 
 ### ======================================================================================================
 ### Use the standard code from the common modules to produce outputs
 ### ======================================================================================================
 #this only needs to be left in for the stock summary plot so run everything 
 #without the line below but then run line below to produce the picture that I want for the stock summary
-#WoS@stock.n[1,ac(2009)] <- NA
+#WoS@stock.n[1,ac(2010)] <- NA
 do.summary.plots(WoS,WoS.ica)
 
 do.retrospective.plots<- function(stck,idxs,ctrl,n.retro.yrs) {
@@ -138,8 +138,8 @@ do.retrospective.plots<- function(stck,idxs,ctrl,n.retro.yrs) {
     stcks   <- do.call(FLStocks,lapply(icas,function(ica) {ica + trim(stck, year=dims(ica@stock.n)$minyear:dims(ica@stock.n)$maxyear)}))
 
     # Remove last years recruitment value as it gets estimated wrong in ICA, replace it later with a GM estimate
-    stcks[[9]]@stock.n[1,ac(2009)] <- NA
-    stcks[[9]]@stock.n[1,ac(2009)] <- exp(mean(log(rec(stck[,ac(1989:2007)])),na.rm=T))
+    stcks[[9]]@stock.n[1,ac(2010)] <- NA
+    stcks[[9]]@stock.n[1,ac(2010)] <- exp(mean(log(rec(stck[,ac(1989:2007)])),na.rm=T))
     #Now call the retro plotting functions
     retro.plots(stcks,icas,ctrl)
 
@@ -168,12 +168,12 @@ print(stacked.area.plot(data~year| unit, as.data.frame(pay(WoS@catch.wt)),groups
 print(stacked.area.plot(data~year,as.data.frame(pay(WoS@stock.n*WoS@stock.wt)),groups="age",main="Proportion by weight in the stock",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
 print(stacked.area.plot(data~year| unit, as.data.frame(pay(WoS.tun[[1]]@index)),groups="age",main="Proportion of Acoustic index at age",ylim=c(-0.01,1.01),xlab="years",col=gray(9:0/9)))
 
-catch.curves(WoS,1990,2009)
+catch.curves(WoS,1990,2010)
 WoS.sr <- ref.pts(WoS,"segreg",100000)
 cor.tun(WoS.tun)
 
 #Time series of west
-west.ts  <- xyplot(data~year,data=window(WoS@stock.wt,1991,2009),
+west.ts  <- xyplot(data~year,data=window(WoS@stock.wt,1991,2010),
               groups=age,
               auto.key=list(space="right",points=FALSE,lines=TRUE,type="b"),
               type="b",
@@ -183,7 +183,7 @@ west.ts  <- xyplot(data~year,data=window(WoS@stock.wt,1991,2009),
 print(west.ts)
 
 #Time series of west by cohort
-west.by.cohort      <- as.data.frame(FLCohort(window(WoS@stock.wt,1992,2009)))
+west.by.cohort      <- as.data.frame(FLCohort(window(WoS@stock.wt,1992,2010)))
 west.by.cohort      <- subset(west.by.cohort,!is.na(west.by.cohort$data))
 west.by.cohort$year <- west.by.cohort$age + west.by.cohort$cohort
 west.cohort.plot    <- xyplot(data~year,data=west.by.cohort,
@@ -224,7 +224,7 @@ FnPrint("PERFORMING SHORT TERM FORECAST...\n")
 #Make forecast
 #WoS recruitment is based on a geometric mean of 1989-2007. Updated by one year, each year
 gm.recs                   <- exp(mean(log(rec(trim(WoS.orig,year=1989:2007)))))
-stf.ctrl                  <- FLSTF.control(nyrs=1,fbar.nyrs=1,fbar.min=3,fbar.max=6,catch.constraint=24420,f.rescale=TRUE,rec=gm.recs)
+stf.ctrl                  <- FLSTF.control(nyrs=1,fbar.nyrs=1,fbar.min=3,fbar.max=6,catch.constraint=22481,f.rescale=TRUE,rec=gm.recs)
 WoS@catch.n[1,52,,,,]     <- 1
 WoS.stf                   <- FLSTF(stock=WoS.orig,control=stf.ctrl,quiet=TRUE,sop.correct=FALSE)
 writeFLStock(WoS.stf,output.file=paste(output.base," WoSaddyr",sep=""))
@@ -249,7 +249,7 @@ FnPrint(paste("COMPLETE IN",sprintf("%0.1f",round(proc.time()[3]-start.time,1)),
 ### ======================================================================================================
 ### Create the figures for the advice sheet and the summary table and reference points
 ### ======================================================================================================
-WoS.sr <- fmle(FLSR(rec=rec(WoS.orig)[,ac(1959:2009)],ssb=ssb(WoS.orig)[,ac(1957:2007)],model="bevholt"),control=list(parscale=c(0.001,1,0.001)))
+WoS.sr <- fmle(FLSR(rec=rec(WoS.orig)[,ac(1959:2010)],ssb=ssb(WoS.orig)[,ac(1957:2007)],model="bevholt"),control=list(parscale=c(0.001,1,0.001)))
 #WoS.sr <- fmle(as.FLSR(transform(WoS,stock.n=WoS@stock.n/100000),model="segreg"));
 WoS.sr@params <- WoS.sr@params*100000
 
