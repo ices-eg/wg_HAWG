ad208f70927ffd325dc1b0d18e86f0a8f10aee08
afraegan <afraegan@gmail.com>
2011-03-22 15:50:27 +0000

    STF added

--- CelticSea/Celtic Sea Assessment.r
+++ CelticSea/Celtic Sea Assessment.r
@@ -51,8 +51,8 @@ source(file.path("..","_Common","HAWG Common assessment module.r"))
 data.source         <-  file.path("data")      #Data source, not code or package source!!!
 output.dir          <-  file.path("res")       #Output directory
 output.base         <-  file.path(output.dir,"cs.herring Assessment") #Output base filename, including directory. Other output filenames are built by appending onto this one
-retro.years         <-  c(2005:2010)  #Specify specific years to do the retrospective over
-#retro.years         <-  c(2003,2005:2009)
+#retro.years         <-  c(2005:2010)  #Specify specific years to do the retrospective over
+retro.years         <-  c(2003,2005:2010)
 ### ======================================================================================================
 ### Output setup
 ### ======================================================================================================
@@ -349,6 +349,104 @@ writeStandardOutput(cs.herring,cs.herring.sr,cs.retro.stck,nyrs.=3,recImY=NULL,o
 
 ##############################################################################################################
 
+### ======================================================================================================
+### Projections
+### ======================================================================================================
+FnPrint("CALCULATING PROJECTIONS...\n")
+
+#Define years
+TaY <- dims(cs.herring)$maxyear   #Terminal assessment year
+ImY <- TaY+1                #Intermediate Year
+AdY <- TaY+2                #Advice year
+CtY <- TaY+3                #Continuation year - not of major concern but used in calculations in places
+tbl.yrs     <- as.character(c(ImY,AdY,CtY))   #Years to report in the output table
+
+#Deal with recruitment - a geometric mean of the five years prior to the terminal assessment year
+rec.years <-     (1995:(cs.herring@range['maxyear']-2))
+gm.recs  <- exp(mean(log(rec(cs.herring)[,as.character(rec.years)]))) # = (prod(rec(WBSS)[,as.character(rec.years)]))^(1/(length(rec.years)))
+cs.herring.srr <- list(model="geomean",params=FLPar(gm.recs))
+
+#Expand stock object
+cs.herring.proj <- stf(cs.herring,nyears=4,wts.nyears=3,arith.mean=TRUE,na.rm=TRUE)
+cs.herring.proj@stock.n[,ac(ImY)]  <- cs.herring.ica@survivors
+cs.herring.proj@stock.n[1,as.character(c(ImY,AdY,CtY))] <- gm.recs
+
+#Define some constants     FOR 2011
+ImY.catch <- 16196
+AdY.catch <- 13200
+numFmsy <- 0.25
+
+
+### 2009 tac 10150
+#2010 TAC  13200
+## Intermediate yr catch 2011 16196
+
+#Setup options
+options.l <- list(#Zero catch
+                  "Catch(2012) = Zero"=
+                    fwdControl(data.frame(year=c(ImY,AdY,CtY),
+                                          quantity="catch",
+                                          val=c(ImY.catch,0,0))),
+                  # TAC, -15% 
+                  "Catch(2012) = 2011 TAC -15% (11220 t)"=
+                    fwdControl(data.frame(year=c(ImY,AdY,CtY),
+                                          quantity=c("catch","catch","f"),
+                                          rel=c(NA,NA,AdY),
+                                          val=c(ImY.catch,AdY.catch*0.85,1))),
+                  #TAC sq
+                  "Catch(2012) = 2011 TAC sq (13200 t)"=
+                    fwdControl(data.frame(year=c(ImY,AdY,CtY),
+                                          quantity=c("catch","catch","f"),
+                                          rel=c(NA,NA,AdY),
+                                          val=c(ImY.catch,AdY.catch*1,1))),
+                  #TAC +15%
+                  "Catch(2012) = 2011 TAC +15% (15180 t)"=
+                    fwdControl(data.frame(year=c(ImY,AdY,CtY),
+                                          quantity=c("catch","catch","f"),
+                                          rel=c(NA,NA,AdY),
+                                          val=c(ImY.catch,AdY.catch*1.15,1))),
+                  # TAC + 25%
+                  "Catch(2012) = 2011 TAC + 25% (16500 t)"=
+                    fwdControl(data.frame(year=c(ImY,AdY,CtY),
+                                          quantity=c("catch","catch","f"),
+                                          rel=c(NA,NA,AdY),
+                                          val=c(ImY.catch, AdY.catch*1.25,1))),
+                  #F =0.25 
+                  "Fbar(2012) = 0.25"=
+                    fwdControl(data.frame(year=c(ImY,AdY,CtY),
+                                          quantity=c("catch","f","f"),
+                                          rel=c(NA,NA,AdY),
+                                          val=c(ImY.catch,numFmsy,1))),
+                  #F =0.19
+                  "Fbar(2012) = 0.19"=
+                    fwdControl(data.frame(year=c(ImY,AdY,CtY),
+                                          quantity=c("catch","f","f"),
+                                          rel=c(NA,NA,AdY),
+                                          val=c(ImY.catch,0.19,1))),
+                  # F = 0.18
+                  "Fbar(2012) = 0.23"=
+                    fwdControl(data.frame(year=c(ImY,AdY,CtY),
+                                          quantity=c("catch","f","f"),
+                                          rel=c(NA,NA,AdY),
+                                          val=c(ImY.catch,0.23,1)))
+) #End options list
+
+
+
+#Multi-options table
+fmult.targs  <- seq(0,2,by=0.1)
+mult.opts.l <- lapply(as.list(fmult.targs),function(fmult) {
+                          fwdControl(data.frame(year=c(ImY,AdY,CtY),
+                                          quantity=c("catch","f","f"),
+                                          rel=c(NA,ImY,AdY),
+                                          val=c(ImY.catch,fmult,1)))
+                  })
+names(mult.opts.l) <- sprintf("Fmult(2010) = %4.3f",fmult.targs)
+
+#Calculate options
+cs.herring.options   <- lapply(options.l,function(ctrl) {fwd(cs.herring.proj,ctrl=ctrl,sr=cs.herring.srr)})
+cs.herring.mult.opts <- lapply(mult.opts.l,function(ctrl) {fwd(cs.herring.proj,ctrl=ctrl,sr=cs.herring.srr)})
+
 
 ### ======================================================================================================
 ### Write Options Tables
@@ -429,15 +527,6 @@ save(cs.herring,cs.herring.stf,cs.herring.tun,cs.herring.ctrl,file=paste(output.
 save.image(file=paste(output.base,"Assessment Workspace.RData"))
 dev.off()
 FnPrint(paste("COMPLETE IN",sprintf("%0.1f",round(proc.time()[3]-start.time,1)),"s.\n\n"))
-
-
-
-
-
-
-
-
-
 
 
 
