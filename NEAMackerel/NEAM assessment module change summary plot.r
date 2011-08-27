1597b74522b1c06c031661567b1a818cf1457443
e.hatfield <e.hatfield@marlab.ac.uk>
2011-08-27 14:14:39 +0000

    year range corrected for summary plot as with Thomas' code

--- NEAMackerel/NEAM assessment module change summary plot.r
+++ NEAMackerel/NEAM assessment module change summary plot.r
@@ -43,7 +43,8 @@ do.summary.plots <- function(stck,ica.obj) {
 cat("GENERATING SUMMARY PLOTS ...\n");flush.console()
 
   #Make stock summary plots (ie SSB, Fbar, recs)
-  summary.data <- as.data.frame(FLQuants(SSB=ssb(stck)[,9:38],"Fbar4-8"=fbar(stck)[,6:38],Recruits=1000*rec(stck),Catch=catch(stck)))
+  ff<-dim(ssb(stck))[2]
+  summary.data <- as.data.frame(FLQuants(SSB=ssb(stck)[,9:ff],"Fbar4-8"=fbar(stck)[,6:ff],Recruits=1000*rec(stck),Catch=catch(stck)))
   scaling.factors <- tapply(summary.data$data,summary.data$qname,function(x) trunc(log10(max(pretty(c(0,x))))/3)*3)
   summary.data$data <- summary.data$data/10^scaling.factors[summary.data$qname]
   ylabels <- apply(cbind(lbl=names(scaling.factors),fctr=scaling.factors),1,function(x) {
