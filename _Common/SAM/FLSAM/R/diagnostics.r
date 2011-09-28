7c36bb5cbce073f91fa7243a79118aa816f56622
mpa <mpa@aqua.dtu.dk>
2011-09-28 14:24:59 +0000

    FLSAM: Added ssass output files so that there is enough there to simulate the output of a run

---
+++ _Common/SAM/FLSAM/R/diagnostics.r
@@ -0,0 +1,14 @@
+### Methods #############################################################
+# diagnostics {{{
+# prepare diagnostic plots from a FLSAM object
+#if (!isGeneric("diagnostics")) {
+#    setGeneric("diagnostics", function(x,...) standardGeneric("diagnostics"))
+#}
+
+#setMethod("diagnostics", signature(x="FLSAM",...),
+#  function(x, ...)
+#  {
+
+#Your plots, start here!
+#Note that the FLSAM object should be called "x"
+
