f9cda98823b0c1351ab66dfe341355650361cbd5
mpa <mpa@aqua.dtu.dk>
2011-09-29 08:47:33 +0000

    FLSAM: v 0.10. A full working version, with FLSAM being called as a single function.

--- _Common/SAM/FLSAM/R/FLSAM.control.r
+++ _Common/SAM/FLSAM/R/FLSAM.control.r
@@ -1,27 +1,31 @@
 setClass("FLSAM.control",
-	representation(
-    range	          ="numeric",   ## minimum and miximum age represented internally in the assessment
-    plus.group      ="logical",   ## should we model the maximum age as a plus group or not?
+ representation(
+    name            ="character",
+    desc            ="character",
+    simulate        ="logical",
+    range	    ="numeric",   ## min and max age represented internally in the assessment
+    plus.group      ="logical",   ## we model the maximum age as a plus group or not?
     states          ="matrix",   ## matrix describing the coupling of the states
-    logN.vars       ="vector",   ## vector of coupling of the logN variances
-    catchabilities  ="matrix",   ## matrix characterising the coupling of the catachability parameters
-    power.law.exps  ="matrix",   ## matrix characterising the coupling of the power law expoonents
+    logN.vars       ="vector",   ## vector coupling the logN variances
+    catchabilities  ="matrix",   ## matrix coupling the catachability parameters
+    power.law.exps  ="matrix",   ## matrix coupling the power law expoonents
     f.vars          ="matrix",   ## matrix of fishing mortality couplings
     obs.vars        ="matrix",   ## matrix coupling the observation variances
     srr             ="integer"),    ## stock recruitment relationship
-	prototype=prototype(
-    range	          =as.numeric(1),   ## minimum age represented internally in the assessment
-    plus.group      =as.logical(TRUE),   ## should we model the maximum age as a plus group or not?
-		states          =as.matrix(0),   ## matrix describing the coupling of the states
-    logN.vars           =as.vector(0),   ## vector of coupling of the logN variables
+  prototype=prototype(
+    simulate        = as.logical(FALSE),
+    range           =as.numeric(1),   ## minimum age represented internally in the assessment
+    plus.group      =as.logical(TRUE),   ## model the maximum age as a plus group?
+    states          =as.matrix(0),   ## matrix describing the coupling of the states
+    logN.vars       =as.vector(0),   ## vector of coupling of the logN variables
     catchabilities  =as.matrix(0),   ## matrix characterising the coupling of the parameters
     power.law.exps  =as.matrix(0),   ## matrix of survey catchabilities
     f.vars          =as.matrix(0),   ## matrix of fishing mortality couplings
     obs.vars        =as.matrix(0),   ## matrix coupling the observation variances
     srr             =as.integer(0)),    ## stock recruitment relationship
-	validity=function(object){
-                	# Everything is fine
-                	return(TRUE)}
+  validity=function(object){
+               	# Everything is fine
+               	return(TRUE)}
 )
 
 FLSAM.control <- function(stck,tun) {
