8cf51aaa7224d264f9b4074ccc4687a3508617b5
mpa <mpa@aqua.dtu.dk>
2011-10-14 10:09:18 +0000

    NSAS: Restructured setup towards a more modular approach. We now have a common script to setup objects, including the default assessment. All other scripts build upon these setups as needs be

--- NSAS/NSH_SAM.r
+++ NSAS/NSH_SAM.r
@@ -6,8 +6,7 @@
 #
 # Author: HAWG model devlopment group
 #
-# Acts as a basic wrapper around the SAM framework for use in the assessment of 
-# NSAS herring. Most of the interfacing is taken care of by the FLR packages
+# Performs an assessment of the NSAS Herring stock using the SAM method
 #
 # Developed with:
 #   - R version 2.13.1
@@ -24,46 +23,16 @@
 ### ============================================================================
 rm(list=ls()); gc(); graphics.off(); start.time <- proc.time()[3]
 options(stringsAsFactors=FALSE)
-FnPrint     <-  function(string) {
-	cat(string)
+log.msg     <-  function(string) {
+	cat(string);flush.console()
 }
-FnPrint("\nNSH SAM Assessment Wrapper\n==========================\n")
+log.msg("\nNSH SAM Assessment\n==========================\n")
 
 ### ============================================================================
 ### Import externals
 ### ============================================================================
 library(FLSAM)
-
-#Load old ICA-based NSH assessessment objects from FLSAM package
-#Normally you would use the FLCore functions to read the data
-#files and create the objects. This is just a shortcut to achieve that
-data(NSH)
-
-### ============================================================================
-### Configure assessment
-### ============================================================================
-#Setup configuration - creates an empty control object with appropriate structure
-NSH.ctrl <- FLSAM.control(NSH,NSH.tun)
-
-#Fishing mortality random walk coupling
-NSH.ctrl@states["catch",] <- c(1:5,rep(6,5))            #Couple age 5+ Fs
-NSH.ctrl@f.vars["catch",] <- 1                          #All have the same variance
-#Log N random walk variances
-NSH.ctrl@logN.vars <- c(1,rep(2,9))
-#Catchability models
-NSH.ctrl@catchabilities["IBTS0","0"] <- 1
-NSH.ctrl@catchabilities["IBTS-Q1",ac(1:5)] <- 2:6
-NSH.ctrl@catchabilities["HERAS",ac(1:9)] <- c(7:10, rep(11,5))    #Set linear catchability model
-#Observation model parameters
-NSH.ctrl@obs.vars["catch",] <- c(1,rep(2,9))
-NSH.ctrl@obs.vars["IBTS0",] <- 3
-NSH.ctrl@obs.vars["IBTS-Q1",] <- 4
-NSH.ctrl@obs.vars["HERAS",] <- 5
-
-#Remove 2010 data to be fully comparable with development version (baserun)
-NSH@catch.n[,"2010"] <- NA
-NSH.tun[["HERAS"]]@index[,"2010"] <- NA
-NSH.tun[["MLAI"]]@index[,"2010"] <- NA
+source("Setup_objects.r")
 
 ### ============================================================================
 ### Run the assessment
@@ -77,6 +46,8 @@ NSH.sam.ass <- NSH + NSH.sam
 ### ============================================================================
 ### Plots
 ### ============================================================================
+pdf(file.path(resdir,"NSH_SAM_assessment.pdf"))
+
 #Survey fits
 #survey.diagnostics(sam.out)
 
@@ -90,7 +61,8 @@ print(p)
 plot(NSH.sam.ass)
 
 ### ============================================================================
-### Compare results
+### Finish
 ### ============================================================================
-save(NSH.sam,NSH.ctrl,NSH.sam.ass,file="NSH.sam.RData")
-FnPrint(paste("COMPLETE IN",sprintf("%0.1f",round(proc.time()[3]-start.time,1)),"s.\n\n"))
+dev.off()
+save(NSH.sam,NSH.ctrl,NSH.sam.ass,file=file.path(resdir,"NSH.sam.RData"))
+log.msg(paste("COMPLETE IN",sprintf("%0.1f",round(proc.time()[3]-start.time,1)),"s.\n\n"))
---
+++ NSAS/Setup_objects.r
@@ -0,0 +1,65 @@
+################################################################################
+# NSH_Setup_Objects
+#
+# $Rev$
+# $Date$
+#
+# Author: HAWG model devlopment group
+#
+# Sets up the data objects (specifically an FLStock and an FLIndices object)
+# necessary to perform a stock assessment. Necessary preprocessing, such as
+# the smoothing of weights, is performed here. Script is intended to be called
+# from other external sources.
+#
+# Developed with:
+#   - R version 2.13.1
+#   - FLCore 2.4
+#
+# To be done:
+#
+# Notes: Have fun running this assessment!
+#
+################################################################################
+
+### ============================================================================
+### Misc
+### ============================================================================
+resdir <- file.path(".","results")
+
+### ============================================================================
+### Setup data objects
+### ============================================================================
+#Load old ICA-based NSH assessessment objects from FLSAM package
+#Normally you would use the FLCore functions to read the data
+#files and create the objects. This is just a shortcut to achieve that
+data(NSH)
+
+#Remove 2010 data to be fully comparable with development version (baserun)
+NSH@catch.n[,"2010"] <- NA
+NSH.tun[["HERAS"]]@index[,"2010"] <- NA
+NSH.tun[["MLAI"]]@index[,"2010"] <- NA
+
+### ============================================================================
+### Setup default assessment configuration
+### ============================================================================
+#Setup configuration - creates an empty control object with appropriate structure
+NSH.ctrl <- FLSAM.control(NSH,NSH.tun)
+
+#Fishing mortality random walk coupling
+NSH.ctrl@states["catch",] <- c(1:5,rep(6,5))            #Couple age 5+ Fs
+NSH.ctrl@f.vars["catch",] <- 1                          #All have the same variance
+
+#Log N random walk variances
+NSH.ctrl@logN.vars <- c(1,rep(2,9))
+
+#Catchability models
+NSH.ctrl@catchabilities["IBTS0","0"] <- 1
+NSH.ctrl@catchabilities["IBTS-Q1",ac(1:5)] <- 2:6
+NSH.ctrl@catchabilities["HERAS",ac(1:9)] <- c(7:10, rep(11,5))    #Set linear catchability model
+
+#Observation model parameters
+NSH.ctrl@obs.vars["catch",] <- c(1,rep(2,9))
+NSH.ctrl@obs.vars["IBTS0","0"] <- 3
+NSH.ctrl@obs.vars["IBTS-Q1",ac(1:5)] <- 4
+NSH.ctrl@obs.vars["HERAS",ac(1:9)] <- 5
+
