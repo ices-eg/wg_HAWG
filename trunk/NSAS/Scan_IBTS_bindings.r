255ad37555fa21a25dfede17fad3f117e9437469
mpa@aqua.dtu.dk <mpa@aqua.dtu.dk@3861ee22-ec89-11dd-a738-5fe2e3cfbcc5>
2011-11-07 15:11:02 +0000

    Added Scan_IBTS_bindings to scan through both catchability and obs_var bindings in IBTS simultaneously

---
+++ trunk/NSAS/Scan_IBTS_bindings.r
@@ -0,0 +1,90 @@
+################################################################################
+# Scan IBTS bindings
+#
+# $Rev$
+# $Date$
+#
+# Author: HAWG model devlopment group
+#
+# The FLSAM model has the ability to bind the observation variances (weightings)
+# and catchabilities of age groups together, effectively using one parameter 
+# for many age groups. 
+# The appropriate bindings can be identified by performing a series of assessments 
+# for each combination and then comparing the results using AIC / LR tests.
+#
+# Developed with:
+#   - R version 2.13.2
+#   - FLCore 2.4
+#
+# To be done:
+#
+# Notes: Have fun running this assessment!
+#
+################################################################################
+
+### ============================================================================
+### Initialise system, including convenience functions and title display
+### ============================================================================
+rm(list=ls()); gc(); graphics.off(); start.time <- proc.time()[3]
+options(stringsAsFactors=FALSE)
+log.msg     <-  function(string) {
+	cat(string);flush.console()
+}
+log.msg("\nScan IBTS bindings\n===========================\n")
+
+### ============================================================================
+### Import externals
+### ============================================================================
+log.msg("IMPORTING EXTERNAL RESOURCES...\n")
+library(FLSAM)
+source("Setup_objects.r")
+source("Setup_default_FLSAM_control.r")
+
+### ============================================================================
+### Modify the default assessment
+### ============================================================================
+#Now scan through the IBTS ages, tying them sequentlly together
+IBTS.ctrls <- list()
+for(i in 1:5) {
+  ctrl <- NSH.ctrl
+  ctrl@obs.vars["IBTS-Q1",ac(i:5)] <- 100
+  ctrl@catchabilities["IBTS-Q1",ac(i:5)] <- 100
+  ctrl@name <- sprintf("%i+",i)
+  ctrl@desc <- sprintf("Age %i+ params bound together",i)
+  IBTS.ctrls[[i]] <- update(ctrl)
+}
+names(IBTS.ctrls) <- sapply(IBTS.ctrls,slot,"name")
+
+### ============================================================================
+### Run the assessment
+### ============================================================================
+#Perform assessment
+IBTS.sams <- lapply(IBTS.ctrls,FLSAM,stck=NSH,tun=NSH.tun,batch.mode=TRUE)
+
+#Convert to FLSAMs
+IBTS.sams <- do.call(FLSAMs,IBTS.sams)
+
+### ============================================================================
+### Analyse the results
+### ============================================================================
+#Drop any that failed to converge
+IBTS <- IBTS.sams[!sapply(IBTS.sams,is.null)]
+
+#Build stock objects
+IBTS.stcks <- do.call(FLStocks,lapply(IBTS,"+",NSH))
+
+#Extract AICs
+IBTS.AICs  <- sapply(IBTS,AIC)
+
+#Plot
+pdf(file.path(resdir,"IBTS_bindings_scan.pdf"))
+plot(IBTS.AICs,main="IBTS",ylab="AIC")
+plot(IBTS.stcks,main="IBTS bindings scan")
+
+dev.off()
+
+### ============================================================================
+### Compare results
+### ============================================================================
+save(IBTS.sams,file=file.path(resdir,"IBTS.sams.RData"))
+log.msg(paste("COMPLETE IN",sprintf("%0.1f",round(proc.time()[3]-start.time,1)),"s.\n\n"))
--- trunk/NSAS/Setup_default_FLSAM_control.r
+++ trunk/NSAS/Setup_default_FLSAM_control.r
@@ -27,7 +27,7 @@
 NSH.ctrl <- FLSAM.control(NSH,NSH.tun)
 
 #Fishing mortality random walk coupling
-NSH.ctrl@states["catch",] <- c(1:5,rep(6,5))            #Couple age 5+ Fs
+NSH.ctrl@states["catch",] <- c(1:7,rep(8,3))            #Couple age 7+ Fs
 NSH.ctrl@f.vars["catch",] <- 1                          #All have the same variance
 
 #Log N random walk variances
@@ -36,19 +36,11 @@ NSH.ctrl@logN.vars <- c(1,rep(2,9))
 #Catchability models
 NSH.ctrl@catchabilities["IBTS0","0"] <- 1
 NSH.ctrl@catchabilities["IBTS-Q1",ac(1:5)] <- 2:6
-#NSH.ctrl@catchabilities["HERAS",ac(1:9)] <- c(7:10, rep(11,5))    #Set linear catchability model
 NSH.ctrl@catchabilities["HERAS",ac(1:9)] <- 7:15
-#NSH.ctrl@catchabilities["MLAI","6"] <- 16
-#NSH.ctrl@power.law.exps["MLAI","6"] <- 1
 
 #Observation model parameters
-#NSH.ctrl@obs.vars["catch",] <- c(1,rep(2,9))
-#NSH.ctrl@obs.vars["IBTS0","0"] <- 3
-#NSH.ctrl@obs.vars["IBTS-Q1",ac(1:5)] <- 4
-#NSH.ctrl@obs.vars["HERAS",ac(1:9)] <- 5
 NSH.ctrl@obs.vars["catch",] <- 1:10
 NSH.ctrl@obs.vars["IBTS0","0"] <- 11
 NSH.ctrl@obs.vars["IBTS-Q1",ac(1:5)] <- 12:16
 NSH.ctrl@obs.vars["HERAS",ac(1:9)] <- 17:25
-#NSH.ctrl@obs.vars["MLAI","6"] <- 6
 
