900c4766717b05a3c16543f5f4272a3851b45bb1
mpa <mpa@aqua.dtu.dk>
2012-01-27 19:13:36 +0000

    NSAS: Added simmonds wts. Relaxed variance of recruitment on all_in run to be more variable than others. Output plots look at weights in more detail

---
+++ NSAS/benchmark/02a_All_in_run.r
@@ -0,0 +1,127 @@
+################################################################################
+# NSH_SAM "All-in" Assessment
+#
+# $Rev: 618 $
+# $Date: 2011-11-11 16:42:31 +0100 (vr, 11 nov 2011) $
+#
+# Author: HAWG model devlopment group
+#
+# Performs an assessment of the NSAS Herring stock using the SAM method including
+# all surveys
+#
+# Developed with:
+#   - R version 2.13.0
+#   - FLCore 2.4
+#
+# To be done:
+#
+# Notes: Have fun running this assessment!
+#
+################################################################################
+
+### ============================================================================
+### Initialise system, including convenience functions and title display
+### ============================================================================
+rm(list=ls()); graphics.off(); start.time <- proc.time()[3]
+options(stringsAsFactors=FALSE)
+log.msg     <-  function(string) {
+	cat(string);flush.console()
+}
+log.msg("\nNSH SAM 'All-in' Assessment\n===========================\n")
+
+### ============================================================================
+### Setup assessment
+### ============================================================================
+#Somewhere to store results
+resdir <- file.path("benchmark","resultsSAM")
+respref <- "02a_All_in" #Prefix for output files
+
+#Import externals
+library(FLSAM)
+source(file.path("benchmark","Setup_objects.r"))
+
+#Exclude MLAI index
+NSH.tun  <- NSH.tun[-which(names(NSH.tun)=="MLAI")] 
+NSH.ctrl <- FLSAM.control(NSH,NSH.tun)
+
+#Modify default settings of control object
+NSH.ctrl@states["catch",] <- seq(dims(NSH)$age) #number at age states move freely
+NSH.ctrl@logN.vars[] <- c(1,rep(2,dims(NSH)$age-1)) #Recruitment gets separate variance
+
+### ============================================================================
+### Run the assessment
+### ============================================================================
+#Perform assessment
+NSH.sam <- FLSAM(NSH,NSH.tun,NSH.ctrl)
+
+#Update stock object
+NSH.sam.ass <- NSH + NSH.sam
+
+### ============================================================================
+### Save results
+### ============================================================================
+save(NSH,NSH.tun,NSH.ctrl,NSH.sam,file=file.path(resdir,paste(respref,".RData",sep="")))
+
+### ============================================================================
+### Plots
+### ============================================================================
+#Setup plots
+pdf(file.path(resdir,paste(respref,".pdf",sep="")))
+
+#Plot result
+NSH.sam@name <- "North Sea Herring All-in Assessment"
+print(plot(NSH.sam))
+
+#Plot catchabilities values
+catch <- catchabilities(NSH.sam)
+print(xyplot(value+ubnd+lbnd ~ age | fleet,catch,
+          scale=list(alternating=FALSE,y=list(relation="free")),as.table=TRUE,
+          type="l",lwd=c(2,1,1),col=c("black","grey","grey"),
+          subset=fleet %in% c("HERAS","IBTS-Q1","IBTS-Q3"),
+          main="Survey catchability parameters",ylab="Catchability",xlab="Age"))
+
+#Plot obs_variance (weightings)
+obv <- obs.var(NSH.sam)
+obv$str <- sprintf("%s_%i",obv$fleet,obv$age)
+obv <- obv[order(obv$value),]
+print(barchart(value ~ sprintf("%s",age)| fleet,obv,
+       col="grey",ylim=range(pretty(c(0,obv$value))),
+       as.table=TRUE,scale=list(alternating=FALSE),horizontal=FALSE,
+       main="Observation Variances",ylab="Observation Variances",xlab="Age"))
+barplot(obv$value,ylab="Observation Variance",names.arg=obv$str,
+       main="Observation variances by survey",col=factor(obv$fleet),las=3)
+
+#Compare weights against Simmonds wts
+sim.wts <- read.csv(file.path(".","data","simmonds_wts.csv"))
+wts <- merge(sim.wts,obv)
+wts$fit.wts <- 1/wts$value
+plot(wts$simmonds_wts,wts$fit.wts,xlab="HAWG 2011 Weightings", 
+  ylab="SAM Fitted Weights",type="n",log="xy",main="Comparison of weightings")
+text(wts$simmonds_wts,wts$fit.wts,wts$str,xpd=NA)
+
+#Plot selectivity pattern over time
+sel.pat <- merge(f(NSH.sam),fbar(NSH.sam),
+             by="year",suffixes=c(".f",".fbar"))
+sel.pat$sel <- sel.pat$value.f/sel.pat$value.fbar
+sel.pat$age <- as.numeric(as.character(sel.pat$age))
+print(xyplot(value.f ~ year,sel.pat,groups=sprintf("Age %02i",age),
+         type="l",as.table=TRUE,auto.key=list(space="right"),
+         main="Fishing pressure over time",xlab="Year",ylab="F",
+         scale=list(alternating=FALSE)))
+print(xyplot(sel ~ year,sel.pat,groups=sprintf("Age %02i",age),
+         type="l",as.table=TRUE,auto.key=list(space="right"),
+         main="Selectivity of the Fishery",xlab="Year",ylab="F/Fbar",
+         scale=list(alternating=FALSE)))
+print(xyplot(sel ~ age|sprintf("%i's",floor(year/5)*5),sel.pat,
+         groups=year,type="l",as.table=TRUE,
+         scale=list(alternating=FALSE),
+         main="Selectivity of the Fishery by Pentad",xlab="Age",ylab="F/Fbar"))
+
+#Survey fits
+residual.diagnostics(NSH.sam)
+
+### ============================================================================
+### Finish
+### ============================================================================
+dev.off()
+log.msg(paste("COMPLETE IN",sprintf("%0.1f",round(proc.time()[3]-start.time,1)),"s.\n\n"))
