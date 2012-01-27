bed8901499554585b5d29ac72af2b19ef681fb3b
mpa@aqua.dtu.dk <mpa@aqua.dtu.dk@3861ee22-ec89-11dd-a738-5fe2e3cfbcc5>
2012-01-27 16:19:40 +0000

    NSAS: Major modifications to Setup_objects and data directory. Simplified reading of indicies. Added reading and polishing of historic data to setup_objects

--- trunk/NSAS/benchmark/Setup_objects.r
+++ trunk/NSAS/benchmark/Setup_objects.r
@@ -24,23 +24,44 @@
 ### ============================================================================
 ### Misc
 ### ============================================================================
-resdir              <- file.path(".","results")
 data.source         <- file.path(".","data")    #Data source, not code or package source!!!
 
 ### ============================================================================
 ### Prepare stock object for assessment
 ### ============================================================================
+#Load object
 NSH                 <- readFLStock(file.path(data.source, "index.txt"),no.discards=TRUE)
 
 #Catch is calculated from: catch.wt * catch.n, however, the reported landings are
 #normally different (due to SoP corrections). Hence we overwrite the calculate landings
 NSH@catch           <- NSH@landings
-units(NSH)[1:17]    <- as.list(c(rep(c("tonnes","thousands","kg"),4), rep("NA",2),"f",rep("NA",2)))
+units(NSH)[1:17]    <- as.list(c(rep(c("tonnes","thousands","kg"),4), 
+                               rep("NA",2),"f",rep("NA",2)))
 
 #Set object details
 NSH@name                              <- "North Sea Herring"
 range(NSH)[c("minfbar","maxfbar")]    <- c(2,6)
 NSH                                   <- setPlusGroup(NSH,NSH@range["max"])
+
+#Historical data is only provided for ages 0-8 prior to 1960 (rather than 0-9 after)
+#We therefore need to fill in the last ages by applying the following assumptions
+#  - weight in the catch and weight in the stock at age 9 is the same as 
+#    in period 1960-1983 (constant in both cases)
+#  - catch at age reported as 8+ is split evenly between age 8 and 9
+#  - natural mortality at age 9 is 0.1 (same as age 8)
+#  - proportion mature at age 9 is 1.0 (same as age 8)
+#  - harvest.spwn and m.spwn are the same as elsewhere
+hist.yrs <- as.character(1947:1959)
+NSH@catch.wt <- NSH@landings.wt #Automatic population of catch.wt introduces NAs 
+NSH@catch.wt["9",hist.yrs] <- 0.271 
+NSH@landings.wt["9",hist.yrs] <- 0.271 
+NSH@catch.n["9",hist.yrs] <- NSH@catch.n["8",hist.yrs]/2
+NSH@catch.n["8",hist.yrs] <- NSH@catch.n["9",hist.yrs]
+NSH@landings.n["9",hist.yrs] <- NSH@landings.n["8",hist.yrs]/2
+NSH@landings.n["8",hist.yrs] <- NSH@landings.n["9",hist.yrs]
+NSH@stock.wt["9",hist.yrs] <- 0.312
+NSH@m["9",hist.yrs] <- 0.1
+NSH@mat["9",hist.yrs] <- 1
 
 #No catches of age 9 in 1977 so stock.wt does not get filled there.
 #Hence, we copy the stock weight for that age from the previous year.
@@ -48,80 +69,67 @@ NSH                                   <- setPlusGroup(NSH,NSH@range["max"])
 #need to use averaging or anything fancier
 NSH@stock.wt["9","1977"]              <- NSH@stock.wt["9","1976"]
 
-#Use mean(y-2,y-1,y) of west_raw to calculate west
+#Use a running mean(y-2,y-1,y) of input wests (i.e. west_raw) to calculate west
 NSH@stock.wt[,3:dim(NSH@stock.wt)[2]] <- (NSH@stock.wt[,3:(dim(NSH@stock.wt)[2]-0)] +
                                           NSH@stock.wt[,2:(dim(NSH@stock.wt)[2]-1)] +
                                           NSH@stock.wt[,1:(dim(NSH@stock.wt)[2]-2)]) / 3
 
-### ============================================================================
-### Read in the new M's
-### ============================================================================
-
-M2            <- read.csv(file.path(".","data","SMoothedM_NotExtrapolated_NSAS.csv"),header=T)
-colnames(M2)  <- sub("X","",colnames(M2))
-rownames(M2)  <- M2[,1]
-M2            <- M2[,-1] #Trim off first column as it contains 'ages'
-M2            <- M2[,apply(M2,2,function(x){all(is.na(x))==F})]
 
 ### ============================================================================
-### Modify the default assessment
+### Prepare Natural Mortality estimates 
 ### ============================================================================
-
-NSHM2       <- NSH
-NSHM2@m[]   <- NA
-yrs         <- dimnames(NSHM2@m)$year
-yrs         <- yrs[which(yrs %in% colnames(M2))]
-NSHM2@m[,yrs][] <- as.matrix(M2)
-
-#- Apply 5 year running average
-extryrs <- dimnames(NSHM2@m)$year[which(!dimnames(NSHM2@m)$year %in% yrs)]
-ages    <- dimnames(NSHM2@m)$age
-extrags <- names(which(apply(M2,1,function(x){all(is.na(x))==T})==T))
-yrAver  <- 5
-for(iYr in as.numeric(rev(extryrs))){
-  for(iAge in ages[!ages%in%extrags]){
-    NSHM2@m[ac(iAge),ac(iYr)] <- yearMeans(NSHM2@m[ac(iAge),ac((iYr+1):(iYr+yrAver)),],na.rm=T)
-  }
-}
-for(iAge in extrags)
-  NSHM2@m[ac(iAge),]          <- NSHM2@m[ac(as.numeric(min(sort(extrags)))-1),]
-  
-#Write new M values into the original stock object
-NSH@m     <- NSHM2@m
+####Read in estimates from external file
+###M2            <- read.csv(file.path(".","data","SMoothedM_NotExtrapolated_NSAS.csv"),
+###                     header=TRUE)
+###colnames(M2)  <- sub("X","",colnames(M2))
+###rownames(M2)  <- M2[,1]
+###M2            <- M2[,-1] #Trim off first column as it contains 'ages'
+###M2            <- M2[,apply(M2,2,function(x){all(is.na(x))==F})]
+###
+####Extract key data from default assessment
+###NSHM2       <- NSH
+###NSHM2@m[]   <- NA
+###yrs         <- dimnames(NSHM2@m)$year
+###yrs         <- yrs[which(yrs %in% colnames(M2))]
+###NSHM2@m[,yrs][] <- as.matrix(M2)
+###
+####- Apply 5 year running average
+###extryrs <- dimnames(NSHM2@m)$year[which(!dimnames(NSHM2@m)$year %in% yrs)]
+###ages    <- dimnames(NSHM2@m)$age
+###extrags <- names(which(apply(M2,1,function(x){all(is.na(x))==T})==T))
+###yrAver  <- 5
+###for(iYr in as.numeric(rev(extryrs))){
+###  for(iAge in ages[!ages%in%extrags]){
+###    NSHM2@m[ac(iAge),ac(iYr)] <- yearMeans(NSHM2@m[ac(iAge),ac((iYr+1):(iYr+yrAver)),],na.rm=T)
+###  }
+###}
+###for(iAge in extrags)
+###  NSHM2@m[ac(iAge),]          <- NSHM2@m[ac(as.numeric(min(sort(extrags)))-1),]
+###  
+####Write new M values into the original stock object
+###NSH@m     <- NSHM2@m
 
 
 ### ============================================================================
 ### Prepare index object for assessment
 ### ============================================================================
-#Load and modify all index data
-NSH.tun             <- readFLIndices(file.path(data.source,"/fleet.txt"),
-                                     file.path(data.source,"/ssb.txt"),type="ICA")
-                           
-#Set names
-NSH.tun[[1]]@name   <- "HERAS"
-NSH.tun[[2]]@name   <- "IBTS-Q1"
-NSH.tun[[3]]@name   <- "IBTS-Q3"
-NSH.tun[[4]]@name   <- "IBTS0"
-NSH.tun[[5]]@name   <- "MLAI"
-names(NSH.tun)      <- lapply(NSH.tun,name)
-
-#Load additional SCAI index
-NSH.scai            <- readFLIndices(file.path(data.source,"/fleet.txt"),
-                                     file.path(data.source,"/scai.txt"),type="ICA")
-#Combine all surveys into one object
-NSH.tot             <- FLIndices()
-for(iSurv in names(NSH.tun))
-  NSH.tot[[iSurv]]  <- NSH.tun[[iSurv]]
-NSH.tot[["SCAI"]]   <- NSH.scai[[which(unlist(lapply(NSH.scai,name))=="SCAI")]]
-NSH.tun             <- FLIndices(NSH.tot)
-
-
-#Set parameters etc
-NSH.tun[["HERAS"]]@type               <- "number"
-NSH.tun[["IBTS-Q1"]]@type             <- "number"
-NSH.tun[["IBTS-Q3"]]@type             <- "number"
-NSH.tun[["IBTS0"]]@type               <- "number"
+#Load and modify all numbers at age data
+NSH.tun   <- readFLIndices(file.path(data.source,"fleet.txt"))
+NSH.tun   <- lapply(NSH.tun,function(x) {x@type <- "number"; return(x)})
 NSH.tun[["IBTS0"]]@range["plusgroup"] <- NA
+
+#Load additional indices derived from IHLS (SCAI, MLAI)
+#Note that there is a bug in readFLIndex which means that we have to
+#take such a round-about approach of loading everything, then dropping
+#the first set of indices. Hopefully will be patched in a future release
+MLAI.idx <-  readFLIndices(file.path(data.source,"fleet.txt"),
+                   file.path(data.source,"mlai.txt"),type="ICA")[-seq(NSH.tun)]
+SCAI.idx <-  readFLIndices(file.path(data.source,"fleet.txt"),
+                   file.path(data.source,"scai.txt"),type="ICA")[-seq(NSH.tun)]
+
+#Combine all surveys into one object
+NSH.tun   <- FLIndices(c(MLAI.idx,SCAI.idx,NSH.tun))
+names(NSH.tun) <- sapply(NSH.tun,name)
 
 #Set a-priori weighting factors from Simmonds 2003. These are included
 #as variances in the indices object
@@ -135,10 +143,9 @@ NSH.tun[["IBTS0"]]@index.var[]  <- 1.0/FLQuant(0.63,
   dimnames=dimnames(NSH.tun[["IBTS0"]]@index)) #MIK
 NSH.tun[["MLAI"]]@index.var[]   <- 1.0/FLQuant(0.60,
   dimnames=dimnames(NSH.tun[["MLAI"]]@index)) #MLAI
-NSH.tun[["MLAI"]]@index.var[]   <- 1.0/FLQuant(0.60,
-  dimnames=dimnames(NSH.tun[["SCAI"]]@index)) #SCAI
+NSH.tun[["SCAI"]]@index.var[]   <- 1.0/FLQuant(0.60,
+  dimnames=dimnames(NSH.tun[["SCAI"]]@index)) #SCAI - assume same as MLAI
 
-#FLICA requires that biomass indices ie MLAI, is the first index
-NSH.tun  <- rev(NSH.tun)
+#FLICA requires that biomass indices ie MLAI, is the first index. Check this is ok
 if (NSH.tun[[1]]@name != "MLAI") warning("MLAI not as the first index")
 
