f8beda700dfbf872ce1b3d5b8606d120cc366953
niels.hintzen@gmail.com <niels.hintzen@gmail.com@3861ee22-ec89-11dd-a738-5fe2e3cfbcc5>
2012-01-31 12:40:09 +0000

    finalized script on natural mortality testing

--- trunk/NSAS/benchmark/03b_natural_mortality.r
+++ trunk/NSAS/benchmark/03b_natural_mortality.r
@@ -52,29 +52,25 @@ SCAI.sam@name <- "SCAI"
 NSH.tun.all <- NSH.tun
 
 ### ============================================================================
-### Run the assessment with MLAI and without any IHLS data first
+### Run the assessment with variable natural mortality and fixed natural mortality
 ### ============================================================================
-#Drop IHLS data, setup new objects, perform assessment
-NSH.tun <- NSH.tun[setdiff(names(NSH.tun),c("SCAI","MLAI"))] 
+#First run with variable natural mortality
 source(file.path("benchmark","03_Setup_selected_surveys.r"))
-noIHLS.sam <- FLSAM(NSH,NSH.tun,NSH.ctrl)
-noIHLS.sam@name <- "no IHLS"
+variable.sam <- FLSAM(NSH,NSH.tun,NSH.ctrl)
+variable.sam@name <- "Variable M"
 
-#Now include the MLAI. The MLAI will only be dropped by "03_Setup_selected_surveys" if
-#it is called "MLAI". We therefore rename it to "temp_MLAI" to avoid this problem
-NSH.tun <- NSH.tun.all
-NSH.tun[["temp_MLAI"]] <- NSH.tun[["MLAI"]]
-NSH.tun <- NSH.tun[setdiff(names(NSH.tun),c("SCAI","MLAI"))]  #Drop SCAI,MLAI 
+#fix natural mortality
+NSH@m[]   <- c(1,1,0.3,0.2,rep(0.1,6))
 source(file.path("benchmark","03_Setup_selected_surveys.r"))
-MLAI.sam <- FLSAM(NSH,NSH.tun,NSH.ctrl)
-MLAI.sam@name <- "MLAI"
+fixed.sam <- FLSAM(NSH,NSH.tun,NSH.ctrl)
+fixed.sam@name <- "Fixed M"
 
 #Combine into one
-IHLS.variations <- FLSAMs(SCAI.sam,MLAI.sam,noIHLS.sam)
-names(IHLS.variations) <- sapply(IHLS.variations,name)
+M.variations <- FLSAMs(variable.sam,fixed.sam)
+names(M.variations) <- sapply(M.variations,name)
 
 #Save any results
-save(NSH,NSH.tun.all,NSH.ctrl,IHLS.variations,
+save(NSH,NSH.tun.all,NSH.ctrl,M.variations,
      file=file.path(resdir,paste(respref,".RData",sep="")))
 
 ### ============================================================================
@@ -84,59 +80,13 @@ save(NSH,NSH.tun.all,NSH.ctrl,IHLS.variations,
 pdf(file.path(resdir,paste(respref,".pdf",sep="")))
 
 #Comparison of assessments with different plus groups
-plot(IHLS.variations,main="Effects of IHLS survey")
-plot(IHLS.variations,xlim=c(1990,2011),
-    main="Effects of IHLS survey over recent time period")
+plot(M.variations,main="Effects of variable natural mortality")
+plot(M.variations,xlim=c(1990,2011),
+    main="Effects of variable vs. fixed natural mortality")
 
-#Prepare datasets for plotting
-indep.ssb <- ssb(IHLS.variations[["no IHLS"]])
-indep.ssb$ssb <- indep.ssb$value
-SCAI <- as.data.frame(NSH.tun.all[["SCAI"]]@index )
-SCAI$SCAI <- SCAI$data
-MLAI <- as.data.frame(NSH.tun.all[["MLAI"]]@index )
-MLAI$MLAI <- MLAI$data
-dat <- merge(indep.ssb[,c("year","ssb")],MLAI[,c("year","MLAI")],all=TRUE)
-dat <- merge(dat,SCAI[,c("year","SCAI")])
-
-#Plot the time series showing MLAI,SCAI
-par(mar=c(5,4,2,4))
-yr.lims <- range(pretty(dat$year))
-plot(dat$year,dat$SCAI,xlab="Year",ylab="SCAI",pch=19,type="p",
-    xaxs="i",yaxs="i",xlim=yr.lims,ylim=range(pretty(dat$SCAI)))
-lines(dat$year,dat$SCAI,lty=1)
-par(new=TRUE)
-plot(dat$year,dat$MLAI,pch=1,type="p",axes=FALSE,xlim=yr.lims,
-   ylim=range(pretty(dat$MLAI)),xlab="",ylab="",xaxs="i",yaxs="i")
-lines(dat$year,dat$MLAI,lty=3)
-axis(4)
-mtext("MLAI",4,line=2)
-legend("topleft",legend=c("SCAI","MLAI"),pch=c(19,1),lty=c(1,3))
-title(main="Comparison of SCAI and MLAI")
-
-#Plot the comparison between MLAI and SCAI
-plot(log(dat$MLAI),log(dat$SCAI),xlab="log MLAI",ylab="log SCAI",pch=19)
-text(log(dat$MLAI),log(dat$SCAI),sprintf("%02i",round(dat$year%%100)),pos=4,xpd=NA)
-mdl <- lm(log(SCAI) ~ log(MLAI),dat)
-abline(mdl)
-title(main="MLAI vs SCAI")
-
-#Plot the comparison between SSB and SCAI
-plot(log(dat$ssb),log(dat$SCAI),xlab="log SSB",ylab="log SCAI",pch=19)
-text(log(dat$ssb),log(dat$SCAI),sprintf("%02i",round(dat$year%%100)),pos=4,xpd=NA)
-mdl <- lm(log(SCAI) ~ log(ssb),dat)
-abline(mdl)
-legend("topleft",bty="n",legend=c(sprintf("r2 = %4.2f",summary(mdl)$r.squared),
-       sprintf("Slope = %4.2f ± %4.2f",coef(mdl)[2],1.96*summary(mdl)$coef[2,2])))
-title(main="SSB vs SCAI")
-
-#Plot the comparison between SSB and MLAI
-plot(log(dat$ssb),log(dat$MLAI),xlab="log SSB",ylab="log MLAI",pch=19)
-text(log(dat$ssb),log(dat$MLAI),sprintf("%02i",round(dat$year%%100)),pos=4,xpd=NA)
-mdl <- lm(log(MLAI) ~ log(ssb),dat)
-abline(mdl)
-legend("topleft",bty="n",legend=c(sprintf("r2 = %4.2f",summary(mdl)$r.squared),
-       sprintf("Slope = %4.2f ± %4.2f",coef(mdl)[2],1.96*summary(mdl)$coef[2,2])))
-title(main="SSB vs MLAI")
+#Plot diagnostics of both runs
+residual.diagnostics(variable.sam)
+residual.diagnostics(fixed.sam)
 
 ### ============================================================================
 ### Finish
