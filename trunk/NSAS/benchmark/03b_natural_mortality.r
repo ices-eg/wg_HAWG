1ed1514a5196fc27fee9ac7485b927345a1a58c0
niels.hintzen@gmail.com <niels.hintzen@gmail.com@3861ee22-ec89-11dd-a738-5fe2e3cfbcc5>
2012-02-02 15:29:26 +0000

    added plot on total mortality

--- trunk/NSAS/benchmark/03b_natural_mortality.r
+++ trunk/NSAS/benchmark/03b_natural_mortality.r
@@ -84,6 +84,14 @@ plot(M.variations,main="Effects of variable natural mortality")
 plot(M.variations,xlim=c(1990,2011),
     main="Effects of variable vs. fixed natural mortality")
 
+#total mortality
+source(file.path("benchmark","Setup_objects.r"))
+mortsVariable <- cbind(as.data.frame(NSH@m + (NSH+M.variations[[1]])@harvest),type="Variable")
+  NSH@m[]     <- c(1,1,0.3,0.2,rep(0.1,6))
+mortsFixed    <- cbind(as.data.frame(NSH@m + (NSH+M.variations[[2]])@harvest),type="Fixed")
+morts         <- rbind(mortsVariable,mortsFixed)
+print(xyplot(data ~ year|as.factor(age),data=morts,type="l",groups=type,ylab="total mortality",main="Total mortality by age",auto.key=T))
+
 #Comparison of weightings
 obvs <- obs.var(M.variations)
 obvs$str <- paste(obvs$fleet,ifelse(is.na(obvs$age),"",obvs$age))
@@ -93,9 +101,12 @@ barplot(as.matrix(obvs.stack),beside=TRUE,las=3,
    main="Variable vs fixed natural mortality",
    ylab="Observation variance")
 
+
 #Plot diagnostics of both runs
 residual.diagnostics(variable.sam)
 residual.diagnostics(fixed.sam)
+
+
 
 ### ============================================================================
 ### Finish
