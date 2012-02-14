35bbc7fc881d780b4d1060a7dc2203f568630bec
niels.hintzen <niels.hintzen@gmail.com>
2012-02-14 12:37:30 +0000

    fix bug in Setup_objects

--- NSAS/benchmark/Setup_objects.r
+++ NSAS/benchmark/Setup_objects.r
@@ -79,15 +79,19 @@ NSH@stock.wt[,3:dim(NSH@stock.wt)[2]] <- (NSH@stock.wt[,3:(dim(NSH@stock.wt)[2]-
 ### Prepare Natural Mortality estimates 
 ### ============================================================================
 #Read in estimates from external file
-if(Massump == "smooth75"){
-  M2            <- read.csv(file.path(".","data","Smoothed_span75_M_NotExtrapolated_NSAS.csv"),
-                     header=TRUE)}
-if(Massump == "smooth50"){
-  M2            <- read.csv(file.path(".","data","Smoothed_span50_M_NotExtrapolated_NSAS.csv"),
-                     header=TRUE)}
-if(Massump == "raw"){
-  M2            <- read.csv(file.path(".","data","Raw_NotExtrapolated_NSAS.csv"),
-                     header=TRUE)}
+M2            <- read.csv(file.path(".","data","Smoothed_span75_M_NotExtrapolated_NSAS.csv"),
+                   header=TRUE)
+if(exists("Massump")==T){
+  if(Massump == "smooth75"){
+    M2            <- read.csv(file.path(".","data","Smoothed_span75_M_NotExtrapolated_NSAS.csv"),
+                       header=TRUE)}
+  if(Massump == "smooth50"){
+    M2            <- read.csv(file.path(".","data","Smoothed_span50_M_NotExtrapolated_NSAS.csv"),
+                       header=TRUE)}
+  if(Massump == "raw"){
+    M2            <- read.csv(file.path(".","data","Raw_NotExtrapolated_NSAS.csv"),
+                       header=TRUE)}
+}
 
 colnames(M2)  <- sub("X","",colnames(M2))
 rownames(M2)  <- M2[,1]
