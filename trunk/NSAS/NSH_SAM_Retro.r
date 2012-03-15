0e150f7c842ef3a9eb98b4cc06f26368f4dd148d
niels.hintzen@gmail.com <niels.hintzen@gmail.com@3861ee22-ec89-11dd-a738-5fe2e3cfbcc5>
2012-03-15 14:25:53 +0000

    finalized NSH_SAM_Assessment assessment code fixed bug in natural mortality calculation in setupAssessmentObjects stock annex setup of setupControlObject Separate Retro file

---
+++ trunk/NSAS/NSH_SAM_Retro.r
@@ -0,0 +1,65 @@
+################################################################################
+# NSH_SAM Assessment
+#
+# $Rev: 697 $
+# $Date: 2012-02-10 09:52:28 +0100 (vr, 10 feb 2012) $
+#
+# Author: HAWG model devlopment group
+#
+# Performs the "Final" assessment for NSAS assessment
+#
+# Developed with:
+#   - R version 2.13.0
+#   - FLCore 2.4
+#
+# To be done:
+#
+# Notes: Have fun running this assessment!
+#
+################################################################################
+
+rm(list=ls()); graphics.off(); start.time <- proc.time()[3]
+options(stringsAsFactors=FALSE)
+log.msg     <-  function(string) {cat(string);}
+log.msg("\nNSH Final Assessment\n=====================\n")
+path <- "~/HAWG/trunk/NSAS/"
+path <- "D:/Repository/HAWG/HAWGrepository/NSAS/"
+try(setwd(path))
+
+### ======================================================================================================
+### Define parameters for use in the assessment code here
+### ======================================================================================================
+output.dir          <-  file.path(".","results")                #Output directory
+output.base         <-  file.path(output.dir,"NSH Assessment")  #Output base filename, including directory. Other output filenames are built by appending onto this one
+n.retro.years       <-  5                                       #Number of years for which to run the retrospective
+
+### ============================================================================
+### Setup assessment
+### ============================================================================
+#Import externals
+library(FLSAM);
+source(file.path("retroResidual.r"))
+
+#Load the output data of the assessment
+res <- try(load(file=file.path(output.dir,"North Sea Herring.RData")))
+if(class(res)=="try-error") stop("Run the assessment first before you can run the retrospective")
+
+#Perform retrospective
+NSH.retro <- retro(NSH,NSH.tun,NSH.ctrl,n.retro.years)
+
+# Save results
+save(NSH.retro,file=file.path(output.dir,paste(name(NSH),"retro.RData",sep="")))
+
+#Setup plots
+pdf(file.path(output.dir,paste(name(NSH),"retro.pdf",sep="")))
+
+#Plot retro
+plot(NSH.retro)
+
+print(retroResiduals(NSH.retro,"HERAS",1990:2011))
+print(retroResiduals(NSH.retro,"catch",1990:2011))
+
+dev.off()
+
+
+
