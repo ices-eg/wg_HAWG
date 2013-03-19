ba177a2dfcaaab3bd7c83592ac320dc8a3d652f5
markpayneatwork <markpayneatwork@3861ee22-ec89-11dd-a738-5fe2e3cfbcc5>
2013-03-19 15:31:37 +0000

    NSAS: Added recruitment diagnostic plots

--- trunk/NSAS/NSH_SAM_Assessment.r
+++ trunk/NSAS/NSH_SAM_Assessment.r
@@ -193,6 +193,12 @@ png(file.path(output.dir,"figures - %02d.png"),units = "px", height=800,width=67
   ### ============================================================================
   ### Various dervied plots
   ### ============================================================================
+  pdf(file.path("results","Recruitment_diagnosics.pdf"),
+      pointsize=16)
+  #Load component data
+  SCAI.dat <- read.csv("data/SCAI/SCAIoutputs/SCAI_indices.csv")
+  SCAI.dat$norSCAI <- rowSums(SCAI.dat[,c("SCAI.OrkShe","SCAI.Buchan","SCAI.Banks")])
+
   #Extract data
   xcols <- c("data","cohort")
   SCAI.df <- as.data.frame(NSH.tun[["SCAI"]]@index) 
@@ -214,39 +220,65 @@ png(file.path(output.dir,"figures - %02d.png"),units = "px", height=800,width=67
   plt.dat$surv.ratio <- plt.dat$data.MIK / plt.dat$data.SCAI
   plt.dat$rps <- plt.dat$data.rec/plt.dat$data.spwn
   plt.dat$yr <- sprintf("%02i",plt.dat$cohort%%100)
+  plt.dat <- merge(plt.dat,SCAI.dat[,c("Year","norSCAI")],
+                 by.x="cohort",by.y="Year",all=TRUE)
+  plt.dat$norSCAI.surv <- plt.dat$data.MIK / plt.dat$norSCAI
+  plt.dat$downs.frac <- 1-plt.dat$norSCAI/plt.dat$data.SCAI
 
   #Time series of recruits-per-spawner
   plot(plt.dat$cohort,plt.dat$rps,log="y",type="n",
     xlab="Spawning Year",ylab="Recruits per spawner")
-  rug(plt.dat$cohort[plt.dat$ssb > 800000],col="black",lwd=2)
-  rug(plt.dat$cohort[plt.dat$ssb > 800000],col="black",side=3,lwd=2)
+  rug(plt.dat$cohort[plt.dat$ssb < 800000],col="black",lwd=2)
+  rug(plt.dat$cohort[plt.dat$ssb < 800000],col="black",side=3,lwd=2)
   points(plt.dat$cohort,plt.dat$rps,pch=16)
   lines(smooth.spline(plt.dat$cohort[-1],plt.dat$rps[-1],penalty=1.4),
     col="red",lwd=2)
-  
+
   #SRR plot
   plot(plt.dat$ssb/1e6,plt.dat$data.rec/1e6,pch=1,
-    xlab="Spawning Stock Biomass (Mt)",ylab="Recruits (10⁹ individuals)") 
+    xlab="Spawning Stock Biomass (Mt)",ylab="Recruits (10⁹ individuals)",
+    log="xy") 
   points(plt.dat$ssb/1e6,plt.dat$data.rec/1e6,
     pch=ifelse(plt.dat$cohort>2001,16,NA),col="red") 
+  points(plt.dat$ssb/1e6,plt.dat$data.rec/1e6,
+       pch=ifelse(plt.dat$cohort==2012,16,NA),col="black") 
+  legend("bottomright",c("Post 2001","2012","Other"),
+          pch=c(16,16,1),col=c("red","black","black"))
+
+  #Residuals in IBTS0
+  res <- subset(residuals(NSH.sam),fleet=="IBTS0")
+  res$cohort <- res$year -1
+  xlims <- range(pretty(res$cohort))
+  oldpar <- par(mfrow=c(2,1),mar=c(0,0,0,0),
+      oma=c(5,4,4,2))
+  plot(res$cohort,res$std.res,type="h",xlim=xlims,
+       xaxt="n",xpd=NA,xlab="",ylab="IBTS0 residual")
+  abline(h=0,col="grey")
+  points(res$cohort,res$std.res,pch=16)
+  abline(lm(std.res ~ cohort,res),col="red")
+  
+  plot(NA,NA,ylim=c(0,1),xlim=xlims,yaxs="i",
+       xlab="Cohort",ylab="Downs Fractions",xpd=NA)
+  with(subset(plt.dat,!is.na(plt.dat$downs.frac)),
+       polygon(c(cohort,rev(cohort)),
+               c(downs.frac,downs.frac*0),
+               col="blue"))
 
   #IBTSO / SCAI time series
+  par(oldpar)
   plot(plt.dat$cohort,plt.dat$surv,log="y",pch=16,
      xlim=range(pretty(plt.dat$cohort[!is.na(plt.dat$surv.ratio)],na.rm=TRUE)),
      xlab="Spawning Year",ylab="Larval Survival (IBTS0/SCAI)")
 
-  #Comparison SCAI vs SSB
-  plot(plt.dat$ssb,plt.dat$data.SCAI,log="xy",
-    xlab="Spawning stock biomass (Mt)",ylab="SCAI",pch=16)
-  yr.idxs <- plt.dat$cohort > 2008 
-  mdl <- lm(log(data.SCAI) ~log(ssb),plt.dat)
-  mdl.dat <- cbind(log.SCAI=predict(mdl),mdl$model)
-  mdl.dat <- mdl.dat[order(mdl.dat[,"log(ssb)"]),]
-  lines(exp(mdl.dat[,"log(ssb)"]),exp(mdl.dat$log.SCAI))
-  text(plt.dat$ssb[yr.idxs],plt.dat$data.SCAI[yr.idxs],
-     plt.dat$yr[yr.idxs],pos=4) 
-  points(plt.dat$ssb[yr.idxs],plt.dat$data.SCAI[yr.idxs],pch=16,col="red")  
-  
+  #IBTS0 / norSCAI time series
+  plot(plt.dat$cohort,plt.dat$norSCAI.surv,log="y",pch=16,
+     xlim=range(pretty(plt.dat$cohort[!is.na(plt.dat$surv.ratio)],na.rm=TRUE)),
+     xlab="Spawning Year",ylab="Northern Component Larval Survival")
+
+
+  dev.off()  
+
+
   ### ============================================================================
   ### Management
   ### ============================================================================
