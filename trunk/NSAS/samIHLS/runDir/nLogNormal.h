277832b869debd24d78fabedd30989b5e680d518
markpayneatwork <markpayneatwork@3861ee22-ec89-11dd-a738-5fe2e3cfbcc5>
2014-03-10 15:36:28 +0000

    And recommitted the updated version again

--- trunk/NSAS/samIHLS/runDir/nLogNormal.h
+++ trunk/NSAS/samIHLS/runDir/nLogNormal.h
@@ -1,13 +1,30 @@
+//  --------------------------------------------------------------------------
+// Copyright (c) 2008,2009,2010,2011,2012, Anders Nielsen <an@aqua.dtu.dk> 
+// and Casper Berg <cbe@aqua.dtu.dk>. All rights reserved.
 // 
-//  ----------------------------------------------------------------------------
-//  "THE BEER-WARE LICENSE" (invented by Poul-Henning Kamp):
-//  Anders Nielsen <an@aqua.dtu.dk> wrote this file. As long as you retain this 
-//  notice you can do whatever you want with this stuff. If we meet some day, 
-//  and you think this stuff is worth it, you can buy me a beer in return. 
-//  ----------------------------------------------------------------------------
-//
-
-// Anders Nielsen <anders.nielsen@hawaii.edu> Sep 2005 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//   * Neither the name of the assessment tool SAM nor the
+//     names of its contributors may be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL ANDERS NIELSEN OR CASPER BERG BE LIABLE 
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+// DAMAGE.
+//  --------------------------------------------------------------------------
 
 #ifndef __nLogNormal_h__
 #define __nLogNormal_h__
@@ -17,8 +34,8 @@
 
 _CONST double log2pi = log(2.0*M_PI);
 
-
-df1b2matrix bksolve(_CONST df1b2matrix& L, _CONST df1b2matrix& b)
+template<class MatrixType>
+MatrixType bksolve(_CONST MatrixType& L, _CONST MatrixType& b)
 {
   RETURN_ARRAYS_INCREMENT();
   int i, k, r, R, c, C;
@@ -26,262 +43,79 @@ df1b2matrix bksolve(_CONST df1b2matrix& L, _CONST df1b2matrix& b)
   R=b.rowmax();
   c=b.colmin();
   C=b.colmax();
-
-  df1b2vector sumVec(c,C);
-  df1b2matrix x(r,R,c,C);
-
+  MatrixType sumVec(1,1,c,C);
+  MatrixType x(r,R,c,C);
   for(i=r; i<=R; ++i){
-    sumVec=b(i);
+    sumVec(1)=b(i);
     for(k=i-1; k>=r; k--){
-      sumVec-=L(i,k)*x(k);
+      sumVec(1)-=L(i,k)*x(k);
     }
-    x(i)=sumVec/L(i,i);
+    x(i)=sumVec(1)/L(i,i);
   }
   RETURN_ARRAYS_DECREMENT();
   return x;
 }
 
-dvar_matrix bksolve(_CONST dvar_matrix& L, _CONST dvar_matrix& b)
+template <class VectorType, class MatrixType>
+  VectorType nLogNormal(_CONST VectorType& x, _CONST MatrixType& mu, _CONST MatrixType& S)
 {
   RETURN_ARRAYS_INCREMENT();
-  int i, k, r, R, c, C;
-  r=b.rowmin();
-  R=b.rowmax();
-  c=b.colmin();
-  C=b.colmax();
-
-  dvar_vector sumVec(c,C);
-  dvar_matrix x(r,R,c,C);
-
-  for(i=r; i<=R; ++i){
-    sumVec=b(i);
-    for(k=i-1; k>=r; k--){
-      sumVec-=L(i,k)*x(k);
-    }
-    x(i)=sumVec/L(i,i);
-  }
-  RETURN_ARRAYS_DECREMENT();
-  return x;
-}
-
-df1b2vector nLogNormal(_CONST df1b2vector& x, _CONST df1b2matrix& mu, _CONST df1b2matrix& S)
-{
-  RETURN_ARRAYS_INCREMENT();	
   int r, R, c, C, N;
   r=mu.rowmin();
   R=mu.rowmax();
   c=mu.colmin();
   C=mu.colmax();
   N=R-r+1;
-  df1b2vector ret(c,C);
-  df1b2matrix diff(r,R,c,C);
+  VectorType ret(c,C);
+  MatrixType diff(r,R,c,C);
   for(int i=c; i<=C; ++i){
     for(int j=r; j<=R; ++j){
       diff(j,i)=x(j)-mu(j,i);
     }
   }
-  df1b2variable logDet=0.0;
-  df1b2matrix chol=choleski_decomp(S);
-  for(int i=r; i<=R; ++i){logDet+=log(chol(i,i));}
-  logDet*=2.0;
-  df1b2matrix tmp=bksolve(chol,diff);
-  ret=0.5*(log2pi*N+logDet+colsum(square(tmp)));
+  VectorType logDet(1,1);
+  logDet(1)=0.0;
+  MatrixType chol=choleski_decomp(S);
+  for(int i=r; i<=R; ++i){logDet(1)+=log(chol(i,i));}
+  logDet(1)*=2.0;
+  MatrixType tmp=bksolve(chol,diff);
+  ret=0.5*(log2pi*N+logDet(1)+colsum(square(tmp)));
   RETURN_ARRAYS_DECREMENT();
   return ret;
 }
 
-dvar_vector nLogNormal(_CONST dvar_vector& x, _CONST dvar_matrix& mu, _CONST dvar_matrix& S)
-{
-  RETURN_ARRAYS_INCREMENT();	
-  int r, R, c, C, N;
-  r=mu.rowmin();
-  R=mu.rowmax();
-  c=mu.colmin();
-  C=mu.colmax();
-  N=R-r+1;
-  dvar_vector ret(c,C);
-  dvar_matrix diff(r,R,c,C);
-  for(int i=c; i<=C; ++i)diff.colfill(i,x-column(mu,i));
-  dvariable logDet;
-  dvar_matrix chol=choleski_decomp(S);
-  logDet=2.0*sum(log(diagonal(chol)));
-  dvar_matrix tmp=bksolve(chol,diff);
-  ret=0.5*(log2pi*N+logDet+colsum(square(tmp)));
-  RETURN_ARRAYS_DECREMENT();
-  return ret;
-}
-
-
-df1b2vector nLogNormal(_CONST df1b2matrix& x, _CONST df1b2vector& mu, _CONST df1b2matrix& S)
+template <class VectorType, class MatrixType>
+  VectorType nLogNormal(_CONST MatrixType& x, _CONST VectorType& mu, _CONST MatrixType& S)
 {
   RETURN_ARRAYS_INCREMENT();
-  df1b2vector ret=nLogNormal(mu, x, S);
+  VectorType ret=nLogNormal(mu, x, S);
   RETURN_ARRAYS_DECREMENT();
   return ret;
 }
-
-dvar_vector nLogNormal(_CONST dvar_matrix& x, _CONST dvar_vector& mu, _CONST dvar_matrix& S)
-{
-  RETURN_ARRAYS_INCREMENT();
-  dvar_vector ret=nLogNormal(mu, x, S);
-  RETURN_ARRAYS_DECREMENT();
-  return ret;
-}
-
 
 df1b2variable nLogNormal(_CONST df1b2vector& x, _CONST df1b2vector& mu, _CONST df1b2matrix& S)
 {
-  RETURN_ARRAYS_INCREMENT();	
+  RETURN_ARRAYS_INCREMENT();
   int r=mu.indexmin(), R=mu.indexmax();
   df1b2matrix MU(r,R,1,1);
   for(int i=r; i<=R; ++i){MU(i,1)=mu(i);}
   df1b2vector tmp=nLogNormal(x, MU, S);
-  df1b2variable ret=tmp(tmp.indexmin());	
-  RETURN_ARRAYS_DECREMENT();
-  return ret;
-}
-dvariable nLogNormal(_CONST dvar_vector& x, _CONST dvar_vector& mu, _CONST dvar_matrix& S)
-{
-  RETURN_ARRAYS_INCREMENT();	
-  dvar_matrix MU(mu.indexmin(),mu.indexmax(),1,1);
-  MU.colfill(1,mu);
-  dvar_vector tmp=nLogNormal(x, MU, S);
-  dvariable ret=tmp(tmp.indexmin());	
+  df1b2variable ret=tmp(tmp.indexmin());
   RETURN_ARRAYS_DECREMENT();
   return ret;
 }
 
-//TEST
-//  dvar_vector nLogNormalChol(_CONST dvar_vector& x, _CONST dvar_matrix& mu, _CONST dvar_matrix& chol)
-//  {
-//    RETURN_ARRAYS_INCREMENT();	
-//    int r, R, c, C, N;
-//    r=mu.rowmin();
-//    R=mu.rowmax();
-//    c=mu.colmin();
-//    C=mu.colmax();
-//    N=R-r+1;
-//    dvar_vector ret(c,C);
-//    dvar_matrix diff(r,R,c,C);
-//    for(int i=c; i<=C; ++i)diff.colfill(i,x-column(mu,i));
-//    dvariable logDet;
-//    logDet=2.0*sum(log(diagonal(chol)));
-//    dvar_matrix tmp=bksolve(chol,diff);
-//    ret=0.5*(log2pi*N+logDet+colsum(square(tmp)));	
-//    RETURN_ARRAYS_DECREMENT();
-//    return ret;
-//  }
-//  
-//  dvar_vector nLogNormalChol(_CONST dvar_matrix& x, _CONST dvar_vector& mu, _CONST dvar_matrix& chol)
-//  {
-//    RETURN_ARRAYS_INCREMENT();	
-//    dvar_vector ret=nLogNormalChol(mu, x, chol);	
-//    RETURN_ARRAYS_DECREMENT();
-//    return ret;
-//  }
-//  
-//  dvariable nLogNormalChol(_CONST dvar_vector& x, _CONST dvar_vector& mu, _CONST dvar_matrix& chol)
-//  {
-//    RETURN_ARRAYS_INCREMENT();	
-//    dvar_matrix MU(mu.indexmin(),mu.indexmax(),1,1);
-//    MU.colfill(1,mu);
-//    dvar_vector tmp=nLogNormalChol(x, MU, chol);
-//    dvariable ret=tmp(tmp.indexmin());	
-//    RETURN_ARRAYS_DECREMENT();
-//    return ret;
-//  }
-//  
-//  void dmdm_fwsolve(void);
-//  
-//  dvar_matrix fwsolve(_CONST dvar_matrix& L, _CONST dvar_matrix& b)
-//  {  
-//    RETURN_ARRAYS_INCREMENT();	
-//    int i, k, r, R, c, C;
-//    r=b.rowmin();
-//    R=b.rowmax();
-//    c=b.colmin();
-//    C=b.colmax();
-//  
-//    dvector sumVec(c,C);
-//    dmatrix xVal(r,R,c,C);
-//    dmatrix LVal=value(L);
-//    dmatrix bVal=value(b);
-//  
-//    for(i=R; i>=r; --i){
-//      sumVec=bVal(i); 
-//      for(k=i+1; k<=R; k++){
-//        sumVec-=LVal(k,i)*xVal(k);
-//      }
-//      xVal(i)=sumVec/LVal(i,i);
-//    }
-//  
-//    dvar_matrix x=nograd_assign(xVal);
-//    save_identifier_string("TEST1");
-//    L.save_dvar_matrix_value();
-//    L.save_dvar_matrix_position();
-//    b.save_dvar_matrix_value();
-//    b.save_dvar_matrix_position();
-//    x.save_dvar_matrix_value();
-//    x.save_dvar_matrix_position();
-//    save_identifier_string("TEST6");
-//    gradient_structure::GRAD_STACK1->
-//             set_gradient_stack(dmdm_fwsolve);	
-//    RETURN_ARRAYS_DECREMENT();
-//    return x;
-//  }
-//  
-//  void dmdm_fwsolve(void)
-//  {
-//    verify_identifier_string("TEST6");
-//    dvar_matrix_position xpos=restore_dvar_matrix_position();
-//    dmatrix dfx=restore_dvar_matrix_derivatives(xpos);
-//    dmatrix x=restore_dvar_matrix_value(xpos);
-//    dvar_matrix_position bpos=restore_dvar_matrix_position();
-//    dmatrix b=restore_dvar_matrix_value(bpos);
-//    dvar_matrix_position Lpos=restore_dvar_matrix_position();
-//    dmatrix L=restore_dvar_matrix_value(Lpos);
-//    verify_identifier_string("TEST1");
-//  
-//    int i, k, r, R, c, C;
-//    r=b.rowmin();  
-//    R=b.rowmax();  
-//    c=b.colmin();
-//    C=b.colmax();
-//  
-//    dmatrix dfL(Lpos);
-//    dmatrix dfb(bpos);
-//    dvector sumVec(c,C);
-//    dvector dfsumVec(c,C);
-//    dfL.initialize();
-//    dfb.initialize();
-//    dfsumVec.initialize();
-//  
-//  
-//    for(i=r; i<=R; ++i){
-//      //bring back sumVec 
-//      sumVec=x(i)*L(i,i);
-//  
-//      //xVal(i)=sumVec/LVal(i,i);
-//      dfsumVec+=dfx(i)/L(i,i);
-//      dfL(i,i)+= -(dfx(i)*sumVec)/(L(i,i)*L(i,i)); 
-//      dfx(i)=0.0;
-//  
-//      for(k=R; k>=i+1; k--){
-//        //sumVec-=LVal(k,i)*xVal(k);
-//        dfL(k,i)-=dfsumVec*x(k);
-//        dfx(k)-=dfsumVec*L(k,i);
-//      }
-//      //sumVec=bVal(i); 
-//      dfb(i)+=dfsumVec; 
-//      dfsumVec=0.0;
-//    }
-//  
-//    dfL.save_dmatrix_derivatives(Lpos);
-//    dfb.save_dmatrix_derivatives(bpos);
-//  }
-//  
-//  
-//  
+dvariable nLogNormal(_CONST dvar_vector& x, _CONST dvar_vector& mu, _CONST dvar_matrix& S)
+{
+  RETURN_ARRAYS_INCREMENT();
+  dvar_matrix MU(mu.indexmin(),mu.indexmax(),1,1);
+  MU.colfill(1,mu);
+  dvar_vector tmp=nLogNormal(x, MU, S);
+  dvariable ret=tmp(tmp.indexmin());
+  RETURN_ARRAYS_DECREMENT();
+  return ret;
+}
+
 #endif
 
 
