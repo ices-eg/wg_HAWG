0c50f6142ad202bf5714ad811f6e62891d40b7da
nielshintzen <niels.hintzen@gmail.com>
2018-03-19 12:26:42 +0100

    update ISH assessment to FLSAM v2

--- IrishSea/Assessment_18/setupControl.r
+++ IrishSea/Assessment_18/setupControl.r
@@ -4,19 +4,20 @@
 ISH.ctrl                                        <- FLSAM.control(ISH,ISH.tun)
 
 #FISHing mortality random walk coupling
-ISH.ctrl@states["catch",]                       <- c(1:6,7,7)               #Couple age 8+ Fs
-ISH.ctrl@f.vars["catch",]                       <- c(rep(1,8))                   #All have the same variance across ages
+ISH.ctrl@states["catch unique",]                       <- c(1:6,7,7)               #Couple age 8+ Fs
+ISH.ctrl@f.vars["catch unique",]                       <- c(1,1,2,2,2,3,4,4)       #All have the same variance across ages
 
 #Log N random walk variances
-ISH.ctrl@logN.vars                              <- c(1,rep(2,7))
+ISH.ctrl@logN.vars                              <- 1
 
 #Catchability models
 ISH.ctrl@catchabilities["AC(VIIaN)",ac(1:8)]    <- c(1:3,rep(4,5))
 
 #Observation model parameters
-ISH.ctrl@obs.vars["catch",ac(1:8)]              <- c(1,2,3,3,4,4,4,4)
-ISH.ctrl@obs.vars["AC(VIIaN)",ac(1:8)]          <- c(1,2,2,3,3,4,4,4)+4 #1-8
+ISH.ctrl@obs.vars["catch unique",ac(1:8)]              <- c(1,2,2,2,3,3,3,3)
+ISH.ctrl@obs.vars["AC(VIIaN)",ac(1:8)]          <- c(4,rep(5,4),rep(6,3))
+ISH.ctrl@obs.vars["VIIaNSpawn",ac(1)]          <- 7
 
 # # Stock recruitment model code (0=RW, 1=Ricker, 2=BH, ... more in time
 ISH.ctrl@srr                                    <- as.integer(0)
-
+ISH.ctrl                                        <- update(ISH.ctrl)
